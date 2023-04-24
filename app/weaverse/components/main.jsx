8a2c333dca84a2f899a400dc13930744bb5829da
hta218 <huynhtuananh21895@gmail.com>
2023-04-24 21:11:35 +0700

    refactor: remove js

--- app/weaverse/components/main.jsx
+++
@@ -1,6 +0,0 @@
-import {forwardRef} from 'react';
-
-let Main = forwardRef((props, ref) => {
-  return <main ref={ref} {...props} />;
-});
-export default Main;
--- app/weaverse/index.jsx
+++
@@ -1,8 +0,0 @@
-import {WeaverseHydrogenRoot} from '@weaverse/hydrogen';
-import components from './components';
-import {useLoaderData} from '@remix-run/react';
-
-export default function RenderWeaverse() {
-  let data = useLoaderData();
-  return <WeaverseHydrogenRoot components={components} data={data} />;
-}
