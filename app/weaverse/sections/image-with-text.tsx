20342214ea01476a34a3f9b7c3c535b021c66101
hta218 <huynhtuananh21895@gmail.com>
2023-05-19 11:21:23 +0700

    chore: update weaverse section

--- app/weaverse/sections/image-with-text.tsx
+++ app/weaverse/sections/image-with-text.tsx
@@ -15,31 +15,33 @@ let ImageWithText = forwardRef<HTMLElement, ImageWithTextProps>(
   (props, ref) => {
     let {loaderData, image, ...rest} = props;
     return (
-      <section className="text-gray-600 body-font" ref={ref} {...rest}>
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            {props.children}
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
-              </button>
+      <section ref={ref} {...rest}>
+        <div className="text-gray-600 body-font bg-slate-300">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              {props.children}
+              <p className="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+              <div className="flex justify-center">
+                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Button
+                </button>
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  Button
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              src={loaderData?.thumbnailUrl || image}
-            />
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src={loaderData?.thumbnailUrl || image}
+              />
+            </div>
           </div>
         </div>
       </section>
