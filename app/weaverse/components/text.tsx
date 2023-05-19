f65a7855caebf883fb9aa550fac6394c81c55a8a
hta218 <huynhtuananh21895@gmail.com>
2023-05-19 11:21:09 +0700

    feat: add hydrogen `Text` component

--- app/weaverse/components/text.tsx
+++ app/weaverse/components/text.tsx
@@ -1,19 +1,35 @@
+import type {
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
+} from '@weaverse/hydrogen';
+import React from 'react';
 import {forwardRef} from 'react';
 
-let Text = forwardRef((props, ref) => {
-  const {children, className} = props;
-  // return (
-  //   <p ref={ref} className={className}>
-  //     {children}
-  //   </p>
-  // );
-  return (
-    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-      hehe
-      <br className="hidden lg:inline-block" />
-      readymade gluten
-    </h1>
-  );
+export interface TextProps extends HydrogenComponentProps {
+  tagName: string;
+  className?: string;
+  value: string;
+}
+
+let Text = forwardRef<HTMLElement, TextProps>((props, ref) => {
+  let {tagName, value, ...rest} = props;
+  return React.createElement(tagName, {ref, ...rest}, value);
 });
+
+Text.defaultProps = {
+  tagName: 'h1',
+  value: 'The quick brown fox jumps over the lazy dog',
+  className: 'title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900',
+};
+
+export let schema: HydrogenComponentSchema = {
+  type: 'text',
+  title: 'Text',
+  inspector: {
+    settings: [],
+    styles: [],
+  },
+  toolbar: ['general-settings', ['duplicate', 'delete']],
+};
 
 export default Text;
