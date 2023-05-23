0d59bfcfd18e80f6e7e07e93bd228255c54df671
hta218 <huynhtuananh21895@gmail.com>
2023-05-23 15:56:24 +0700

    feat: add testimonial section

--- app/weaverse/sections/index.ts
+++ app/weaverse/sections/index.ts
@@ -1,5 +1,7 @@
 import {imageWithText} from './image-with-text';
+import {testimonial} from './testimonial';
 
 export let sections = {
   ...imageWithText,
+  ...testimonial,
 };
---
+++ app/weaverse/sections/testimonial/_index.tsx
@@ -0,0 +1,48 @@
+import type {
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
+} from '@weaverse/hydrogen';
+import {forwardRef} from 'react';
+
+type TestimonialData = {
+  heading: string;
+};
+
+let Testimonial = forwardRef<
+  HTMLElement,
+  HydrogenComponentProps<TestimonialData>
+>((props, ref) => {
+  let {data, children, ...rest} = props;
+  let {heading} = data;
+  return (
+    <section ref={ref} {...rest} className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <h1 className="text-3xl font-medium title-fon mb-12 text-center text-white">
+          {heading}
+        </h1>
+        <div className="flex flex-wrap -m-4">{children}</div>
+      </div>
+    </section>
+  );
+});
+
+export default Testimonial;
+
+Testimonial.defaultProps = {
+  data: {
+    heading: 'Testimonials',
+  },
+};
+
+export let schema: HydrogenComponentSchema = {
+  type: 'testimonial',
+  title: 'Testimonial',
+  inspector: {
+    settings: [],
+    styles: [],
+  },
+  toolbar: ['general-settings', ['duplicate', 'delete']],
+  flags: {
+    isSection: true,
+  },
+};
