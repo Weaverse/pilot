6aa89c2f3622bd852637f8a3b543568d29dd0229
hta218 <huynhtuananh21895@gmail.com>
2023-05-23 10:09:34 +0700

    chore: update Text element

--- app/weaverse/components/text.tsx
+++ app/weaverse/components/text.tsx
@@ -5,22 +5,24 @@ import type {
 import React from 'react';
 import {forwardRef} from 'react';
 
-export interface TextProps extends HydrogenComponentProps {
-  tagName: string;
-  className?: string;
-  text: string;
-}
+export interface TextProps
+  extends HydrogenComponentProps<{
+    tagName: string;
+    text: string;
+  }> {}
 
 let Text = forwardRef<HTMLElement, TextProps>((props, ref) => {
-  console.log('👉 --------> - props:', props)
-  let {text, tagName, ...rest} = props;
+  let {data, ...rest} = props;
+  let {tagName, text} = data;
   return React.createElement(tagName, {ref, ...rest}, text);
 });
 
 Text.defaultProps = {
-  tagName: 'h1',
-  text: 'The quick brown fox jumps over the lazy dog hehe',
-  className: 'title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900',
+  data: {
+    tagName: 'h1',
+    text: 'The quick brown fox jumps over the lazy dog hehe',
+    className: 'title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900',
+  },
 };
 
 export let schema: HydrogenComponentSchema = {
