93d9699421bccf1ebeb11e3bcb372452050b8190
Paul <phanminh65@gmail.com>
2023-06-19 17:59:17 +0700

    chore: update remix.config

--- remix.config.js
+++ remix.config.js
@@ -17,11 +17,13 @@ module.exports = {
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
   future: {
-    unstable_postcss: true,
-    unstable_tailwind: true,
     v2_meta: true,
     v2_routeConvention: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
+    v2_headers: true,
   },
+
+  postcss: true,
+  tailwind: true,
 };
