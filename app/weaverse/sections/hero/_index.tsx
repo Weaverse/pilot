b129af254d34676f7de207b85180e0d933073230
hta218 <huynhtuananh21895@gmail.com>
2023-07-08 15:10:43 +0700

    chore(weaverse): update `Hero`

--- app/weaverse/sections/hero/_index.tsx
+++ app/weaverse/sections/hero/_index.tsx
@@ -21,61 +21,63 @@ interface HeroProps
 
 let Hero = forwardRef<HTMLElement, HeroProps>((props, ref) => {
   let {loaderData, height, loading, top, collectionHandle, ...rest} = props;
-  let {byline, cta, handle, heading, spread, spreadSecondary} =
-    loaderData || {};
-  return (
-    <section ref={ref} {...rest}>
-      <Link to={`/collections/${handle}`}>
-        <div
-          className={clsx(
-            'relative justify-end flex flex-col w-full',
-            top && '-mt-nav',
-            height === 'full'
-              ? 'h-screen'
-              : 'aspect-[4/5] sm:aspect-square md:aspect-[5/4] lg:aspect-[3/2] xl:aspect-[2/1]',
-          )}
-        >
-          <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
-            {spread?.reference && (
-              <div>
-                <SpreadMedia
-                  sizes={
-                    spreadSecondary?.reference
-                      ? '(min-width: 48em) 50vw, 100vw'
-                      : '100vw'
-                  }
-                  data={spread.reference as Media}
-                  loading={loading}
-                />
-              </div>
+  if (loaderData) {
+    let {byline, cta, handle, heading, spread, spreadSecondary} = loaderData;
+    return (
+      <section ref={ref} {...rest}>
+        <Link to={`/collections/${handle}`}>
+          <div
+            className={clsx(
+              'relative justify-end flex flex-col w-full',
+              top && '-mt-nav',
+              height === 'full'
+                ? 'h-screen'
+                : 'aspect-[4/5] sm:aspect-square md:aspect-[5/4] lg:aspect-[3/2] xl:aspect-[2/1]',
             )}
-            {spreadSecondary?.reference && (
-              <div className="hidden md:block">
-                <SpreadMedia
-                  sizes="50vw"
-                  data={spreadSecondary.reference as Media}
-                  loading={loading}
-                />
-              </div>
-            )}
+          >
+            <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
+              {spread?.reference && (
+                <div>
+                  <SpreadMedia
+                    sizes={
+                      spreadSecondary?.reference
+                        ? '(min-width: 48em) 50vw, 100vw'
+                        : '100vw'
+                    }
+                    data={spread.reference as Media}
+                    loading={loading}
+                  />
+                </div>
+              )}
+              {spreadSecondary?.reference && (
+                <div className="hidden md:block">
+                  <SpreadMedia
+                    sizes="50vw"
+                    data={spreadSecondary.reference as Media}
+                    loading={loading}
+                  />
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col items-baseline justify-between gap-4 px-6 py-8 sm:px-8 md:px-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
+              {heading?.value && (
+                <Heading format as="h2" size="display" className="max-w-md">
+                  {heading.value}
+                </Heading>
+              )}
+              {byline?.value && (
+                <Text format width="narrow" as="p" size="lead">
+                  {byline.value}
+                </Text>
+              )}
+              {cta?.value && <Text size="lead">{cta.value}</Text>}
+            </div>
           </div>
-          <div className="flex flex-col items-baseline justify-between gap-4 px-6 py-8 sm:px-8 md:px-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
-            {heading?.value && (
-              <Heading format as="h2" size="display" className="max-w-md">
-                {heading.value}
-              </Heading>
-            )}
-            {byline?.value && (
-              <Text format width="narrow" as="p" size="lead">
-                {byline.value}
-              </Text>
-            )}
-            {cta?.value && <Text size="lead">{cta.value}</Text>}
-          </div>
-        </div>
-      </Link>
-    </section>
-  );
+        </Link>
+      </section>
+    );
+  }
+  return null;
 });
 
 export default Hero;
@@ -108,7 +110,12 @@ export let schema: HydrogenComponentSchema = {
           configs: {
             options: [
               {label: 'Eager', value: 'eager', icon: 'Lightning'},
-              {label: 'Lazy', value: 'lazy', icon: 'SpinnerGap'},
+              {
+                label: 'Lazy',
+                value: 'lazy',
+                icon: 'SpinnerGap',
+                weight: 'light',
+              },
             ],
           },
           helpText:
