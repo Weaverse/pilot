10c8b3f80b39be000e99733209ac3a9c7cc51c27
hta218 <huynhtuananh21895@gmail.com>
2023-07-26 16:59:42 +0700

    feat: add collection & product fragments

--- app/data/fragments.ts
+++ app/data/fragments.ts
@@ -1,4 +1,4 @@
-export const MEDIA_FRAGMENT = `#graphql
+export let MEDIA_FRAGMENT = `#graphql
   fragment Media on Media {
     __typename
     mediaContentType
@@ -37,7 +37,7 @@ export const MEDIA_FRAGMENT = `#graphql
   }
 `;
 
-export const PRODUCT_CARD_FRAGMENT = `#graphql
+export let PRODUCT_CARD_FRAGMENT = `#graphql
   fragment ProductCard on Product {
     id
     title
@@ -74,7 +74,7 @@ export const PRODUCT_CARD_FRAGMENT = `#graphql
   }
 `;
 
-export const FEATURED_COLLECTION_FRAGMENT = `#graphql
+export let FEATURED_COLLECTION_FRAGMENT = `#graphql
   fragment FeaturedCollectionDetails on Collection {
     id
     title
@@ -84,6 +84,71 @@ export const FEATURED_COLLECTION_FRAGMENT = `#graphql
       width
       height
       url
+    }
+  }
+`;
+
+export let COLLECTION_CONTENT_FRAGMENT = `#graphql
+  fragment CollectionContent on Collection {
+    id
+    handle
+    title
+    descriptionHtml
+    heading: metafield(namespace: "hero", key: "title") {
+      value
+    }
+    byline: metafield(namespace: "hero", key: "byline") {
+      value
+    }
+    cta: metafield(namespace: "hero", key: "cta") {
+      value
+    }
+    spread: metafield(namespace: "hero", key: "spread") {
+      reference {
+        ...Media
+      }
+    }
+    spreadSecondary: metafield(namespace: "hero", key: "spread_secondary") {
+      reference {
+        ...Media
+      }
+    }
+  }
+  ${MEDIA_FRAGMENT}
+`;
+
+export let PRODUCT_VARIANT_FRAGMENT = `#graphql
+  fragment ProductVariantFragment on ProductVariant {
+    id
+    availableForSale
+    selectedOptions {
+      name
+      value
+    }
+    image {
+      id
+      url
+      altText
+      width
+      height
+    }
+    price {
+      amount
+      currencyCode
+    }
+    compareAtPrice {
+      amount
+      currencyCode
+    }
+    sku
+    title
+    unitPrice {
+      amount
+      currencyCode
+    }
+    product {
+      title
+      handle
     }
   }
 `;
