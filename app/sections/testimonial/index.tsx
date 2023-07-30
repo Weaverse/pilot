5028976ce0608afebc9f3bb231ebd56cab428a5f
hta218 <huynhtuananh21895@gmail.com>
2023-07-30 17:23:07 +0700

    chore: update testimonial section

--- app/sections/testimonial/index.tsx
+++ app/sections/testimonial/index.tsx
@@ -6,17 +6,41 @@ import {forwardRef} from 'react';
 
 interface TestimonialProps extends HydrogenComponentProps {
   heading: string;
+  description: string;
 }
 
 let Testimonial = forwardRef<HTMLElement, TestimonialProps>((props, ref) => {
-  let {heading, children, ...rest} = props;
+  let {heading, description, children, ...rest} = props;
   return (
-    <section ref={ref} {...rest} className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <h1 className="text-3xl font-medium title-fon mb-12 text-center text-white">
-          {heading}
-        </h1>
-        <div className="flex flex-wrap -m-4">{children}</div>
+    <section ref={ref} {...rest} className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-md text-center">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            {heading}
+          </h2>
+          {description && (
+            <p className="mb-8 font-light text-gray-500 lg:mb-16 dark:text-gray-400 sm:text-xl">
+              {description}
+            </p>
+          )}
+        </div>
+        <div className="grid gap-8 lg:grid-cols-3">
+          <div className="space-y-6">
+            {children?.map((child, idx) => {
+              if (idx % 3 === 0) return child;
+            })}
+          </div>
+          <div className="space-y-6">
+            {children?.map((child, idx) => {
+              if (idx % 3 === 1) return child;
+            })}
+          </div>
+          <div className="space-y-6">
+            {children?.map((child, idx) => {
+              if (idx % 3 === 2) return child;
+            })}
+          </div>
+        </div>
       </div>
     </section>
   );
@@ -36,8 +60,16 @@ export let schema: HydrogenComponentSchema = {
           type: 'text',
           name: 'heading',
           label: 'Heading',
-          placeholder: 'Section heading',
           defaultValue: 'Testimonials',
+          placeholder: 'Testimonials',
+        },
+        {
+          type: 'textarea',
+          name: 'description',
+          label: 'Description',
+          defaultValue:
+            'Hear from real customers about their experiences shopping with us. From fast shipping and easy returns to quality products and excellent service, see why customers love buying from our store.',
+          placeholder: 'Optional description',
         },
       ],
     },
@@ -47,9 +79,60 @@ export let schema: HydrogenComponentSchema = {
     children: [
       {
         type: 'testimonial--item',
+        heading: 'Reliable shipping for every order',
+        content:
+          'This online store has a very robust and beautiful collection of products. Under the hood it integrates seamlessly with the best ecommerce platforms and fulfillment services.',
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+        authorName: 'Bonnie Green',
+        authorTitle: 'Shipping Manager at Acme Co.',
       },
       {
         type: 'testimonial--item',
+        heading: 'Fast delivery across the country',
+        content:
+          'I order from this store weekly and my packages always arrive on time. Their logistics network ensures speedy delivery no matter where I am.',
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png',
+        authorName: 'James Davis',
+        authorTitle: 'Repeat Customer',
+      },
+      {
+        type: 'testimonial--item',
+        heading: 'User-friendly online shopping',
+        content:
+          'As someone new to online shopping, I found this store so easy to navigate. Checking out is a breeze and customer service is very responsive.',
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png',
+        authorName: 'Sarah Kim',
+        authorTitle: 'First-time Online Shopper',
+      },
+      {
+        type: 'testimonial--item',
+        heading: 'Top-notch customer service',
+        content: `I had an issue with an order and their customer service resolved it quickly and pleasantly. I'll definitely shop here again due to their support.`,
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png',
+        authorName: 'Michael Davis',
+        authorTitle: 'Satisfied Customer',
+      },
+      {
+        type: 'testimonial--item',
+        heading: 'High-quality products',
+        content: `Everything I've purchased from this store has been top-notch. The products are made well and worth the price.`,
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/helene-engels.png',
+        authorName: 'Jessica Lee',
+        authorTitle: 'Repeat Customer',
+      },
+      {
+        type: 'testimonial--item',
+        heading: 'Fast shipping worldwide',
+        content: `I've ordered from this store from multiple countries and always receive my packages quicker than expected.`,
+        authorImage:
+          'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png',
+        authorName: 'John Smith',
+        authorTitle: 'Global Customer',
       },
     ],
   },
--- app/sections/testimonial/item.tsx
+++ app/sections/testimonial/item.tsx
@@ -5,6 +5,7 @@ import type {
 import {forwardRef} from 'react';
 
 interface TestimonialItemProps extends HydrogenComponentProps {
+  heading: string;
   content: string;
   authorImage: string;
   authorName: string;
@@ -13,33 +14,31 @@ interface TestimonialItemProps extends HydrogenComponentProps {
 
 let TestimonialItem = forwardRef<HTMLDivElement, TestimonialItemProps>(
   (props, ref) => {
-    let {content, authorImage, authorName, authorTitle, ...rest} = props;
+    let {heading, content, authorImage, authorName, authorTitle, ...rest} =
+      props;
     return (
-      <div ref={ref} {...rest} className="p-4 md:w-1/2 w-full">
-        <div className="h-full bg-gray-100 p-8 rounded">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="block w-5 h-5 text-gray-400 mb-4"
-            viewBox="0 0 975.036 975.036"
-          >
-            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-          </svg>
-          <p className="leading-relaxed mb-6">{content}</p>
-          <a className="inline-flex items-center" href="/">
+      <div ref={ref} {...rest}>
+        <figure className="p-6 bg-gray-50 rounded dark:bg-gray-800">
+          <blockquote className="text-sm text-gray-500 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              {heading}
+            </h3>
+            <p className="my-4">"{content}"</p>
+          </blockquote>
+          <figcaption className="flex items-center space-x-3">
             <img
-              alt="testimonial"
+              className="w-9 h-9 rounded-full"
               src={authorImage}
-              className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+              alt="author profile picture"
             />
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="title-font font-medium text-gray-900">
-                {authorName}
-              </span>
-              <span className="text-gray-500 text-sm">{authorTitle}</span>
-            </span>
-          </a>
-        </div>
+            <div className="space-y-0.5 font-medium dark:text-white">
+              <div>{authorName}</div>
+              <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+                {authorTitle}
+              </div>
+            </div>
+          </figcaption>
+        </figure>
       </div>
     );
   },
@@ -56,31 +55,38 @@ export let schema: HydrogenComponentSchema = {
       inputs: [
         {
           type: 'text',
+          name: 'heading',
+          label: 'Heading',
+          defaultValue: 'Reliable international shipping',
+          placeholder: 'Testimonial heading',
+        },
+        {
+          type: 'text',
           name: 'content',
           label: 'Content',
+          defaultValue: `I've ordered to multiple countries without issue. Their calculated duties/taxes and import fees make international delivery transparent.`,
           placeholder: 'Testimonial content',
-          defaultValue: 'Testimonial content',
         },
         {
           type: 'image',
           name: 'authorImage',
           label: 'Author image',
           defaultValue:
-            'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&w=300&q=80',
+            'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png',
         },
         {
           type: 'text',
           name: 'authorName',
           label: 'Author Name',
+          defaultValue: 'Emma Thomas',
           placeholder: 'Author name',
-          defaultValue: 'Author name',
         },
         {
           type: 'text',
           name: 'authorTitle',
           label: 'Author Title',
+          defaultValue: 'International Customer',
           placeholder: 'Author title',
-          defaultValue: 'Author title',
         },
       ],
     },
