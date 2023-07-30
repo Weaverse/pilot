04366cca6ee97c9a6686b8f0b26b4b67b73f42e4
hta218 <huynhtuananh21895@gmail.com>
2023-07-30 09:25:27 +0700

    feat: add page details query

--- app/data/queries.ts
+++ app/data/queries.ts
@@ -394,3 +394,18 @@ export let ARTICLE_QUERY = `#graphql
     }
   }
 `;
+
+export const PAGE_QUERY = `#graphql
+  query PageDetails($language: LanguageCode, $handle: String!)
+  @inContext(language: $language) {
+    page(handle: $handle) {
+      id
+      title
+      body
+      seo {
+        description
+        title
+      }
+    }
+  }
+`;
