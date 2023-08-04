4f8023c4a72059c228bae28c7cc1f3af6955bdae
hta218 <huynhtuananh21895@gmail.com>
2023-08-04 16:19:25 +0700

    feat(weaverse): add weaverse studio

--- app/weaverse/index.tsx
+++ app/weaverse/index.tsx
@@ -1,9 +1,17 @@
-import {WeaverseHydrogenRoot} from '@weaverse/hydrogen';
+import {HydrogenStudio, WeaverseHydrogenRoot} from '@weaverse/hydrogen';
 import {components} from './components';
 import {themeSchema} from './theme-schema';
+import {GenericError} from '~/components/GenericError';
 
 export function WeaverseContent() {
   return (
-    <WeaverseHydrogenRoot components={components} themeSchema={themeSchema} />
+    <WeaverseHydrogenRoot
+      components={components}
+      ErrorComponent={GenericError}
+    />
   );
+}
+
+export function WeaverseStudio() {
+  return <HydrogenStudio themeSchema={themeSchema} />;
 }
