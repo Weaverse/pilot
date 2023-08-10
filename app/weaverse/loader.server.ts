fba0610a334c6c70f7caa806ddcb2129af429b39
hta218 <huynhtuananh21895@gmail.com>
2023-08-10 10:20:03 +0700

    refactor: update weaverse content & clean up

--- app/weaverse/loader.server.ts
+++ app/weaverse/loader.server.ts
@@ -1,21 +1,11 @@
 import {LoaderArgs} from '@shopify/remix-oxygen';
-import {
-  WeaverseLoaderConfigs,
-  studioCheck,
-  weaverseLoader,
-} from '@weaverse/hydrogen';
+import {WeaverseLoaderConfigs, weaverseLoader} from '@weaverse/hydrogen';
 import {countries} from '~/data/countries';
 import {components} from './components';
-import {schema} from './schema.server';
 
 export async function loadWeaversePage(
   args: LoaderArgs,
   configs?: WeaverseLoaderConfigs,
 ) {
   return await weaverseLoader(args, components, countries, configs);
-}
-
-export async function getWeaverseThemeConfigs(request: Request) {
-  let isStudio = studioCheck(request);
-  return isStudio ? {schema, countries} : null;
 }
--- app/weaverse/theme-schema.ts
+++ app/weaverse/theme-schema.ts
@@ -1,9 +1,9 @@
 import type {HydrogenThemeSchema} from '@weaverse/hydrogen';
 
-export let schema: HydrogenThemeSchema = {
+export let themeSchema: HydrogenThemeSchema = {
   info: {
     name: 'Pilot',
-    version: '1.0.0',
+    version: '2.1.0',
     author: 'Weaverse',
     authorProfilePhoto:
       'https://ucarecdn.com/174c3d08-fc53-4088-8d12-8eaf7090cdec/',
