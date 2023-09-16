ad26d5ef8154e988cade4441da05e3ffc624ed8e
hta218 <huynhtuananh21895@gmail.com>
2023-09-16 07:58:40 +0700

    chore: remove unused section

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -1,33 +1,27 @@
 import type {HydrogenComponent} from '@weaverse/hydrogen';
+import * as AllProducts from '~/sections/all-products';
+import * as BlogPost from '~/sections/blog-post';
+import * as Blogs from '~/sections/blogs';
 import * as CollectionFilters from '~/sections/collection-filters';
 import * as CollectionList from '~/sections/collection-list';
 import * as FeaturedCollections from '~/sections/featured-collections';
 import * as FeaturedProducts from '~/sections/featured-products';
 import * as Hero from '~/sections/hero';
-import * as Cart from '~/sections/cart';
-import * as AllProducts from '~/sections/all-products';
 import * as ImageGallery from '~/sections/image-gallery';
 import * as ImageGalleryItem from '~/sections/image-gallery/image';
 import * as Main from '~/sections/main';
-import * as Search from '~/sections/search';
+import * as Page from '~/sections/page';
 import * as ProductInformation from '~/sections/product-information';
 import * as RelatedProducts from '~/sections/related-products';
+import * as SingleProduct from '~/sections/single-product';
 import * as Testimonial from '~/sections/testimonial';
 import * as TestimonialItem from '~/sections/testimonial/item';
-import * as CustomerLogin from '~/sections/customer/login';
-import * as CustomerRegister from '~/sections/customer/register';
-import * as Page from '~/sections/page';
 import * as Video from '~/sections/video';
-import * as Blogs from '~/sections/blogs';
-import * as BlogPost from '~/sections/blog-post';
-import * as SingleProduct from '~/sections/single-product';
 
 export let components: HydrogenComponent[] = [
   Main,
-  Cart,
   Hero,
   Page,
-  Search,
   Video,
   Blogs,
   BlogPost,
@@ -42,7 +36,5 @@ export let components: HydrogenComponent[] = [
   RelatedProducts,
   CollectionFilters,
   CollectionList,
-  CustomerLogin,
-  CustomerRegister,
   SingleProduct,
 ];
