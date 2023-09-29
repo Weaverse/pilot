d2d7c1c2a85c85600f2d246473fee69c2f9f8768
dangthang1903 <dangthangkk2000@gmail.com>
2023-09-29 15:04:02 +0700

    refactor: Update functionality for section

--- app/sections/collection-header.tsx
+++ app/sections/collection-header.tsx
@@ -1,142 +1,140 @@
 import type {
-    HydrogenComponentProps,
-    HydrogenComponentSchema,
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
 } from '@weaverse/hydrogen';
 import { forwardRef } from 'react';
 import { useLoaderData } from '@remix-run/react';
 import { CollectionDetailsQuery } from 'storefrontapi.generated';
+import { CSSProperties } from 'react';
+import clsx from 'clsx';
 
 interface HeaderProps extends HydrogenComponentProps {
-    heightSectionDesktop: number;
-    heightSectionMobile: number;
-    enableBackground: boolean;
-    textPosition: string;
-    overlayBackground: number;
-    enableOverlay: boolean;
+  sectionHeightDesktop: number;
+  sectionHeightMobile: number;
+  enableBackground: boolean;
+  overlayOpacity: number;
+  enableOverlay: boolean;
+  contentPosition: string;
 }
 
 let CollectHeader = forwardRef<HTMLElement, HeaderProps>((props, ref) => {
-    let { heightSectionDesktop, heightSectionMobile, enableBackground, textPosition, overlayBackground, enableOverlay, ...rest } = props;
-    let { collection } = useLoaderData<
-        CollectionDetailsQuery & {
-            collections: Array<{ handle: string; title: string }>;
-        }
-    >();
-    let backgroundStyle = {};
-    if (enableBackground) {
-        backgroundStyle = {
-            backgroundImage: `url('${collection?.image?.url}')`,
-            '--header-height-desktop': `${heightSectionDesktop}px`,
-            '--header-height-mobile': `${heightSectionMobile}px`
-        };
+  let { sectionHeightDesktop, sectionHeightMobile, enableBackground, overlayOpacity, enableOverlay, contentPosition, ...rest } = props;
+  let { collection } = useLoaderData<
+    CollectionDetailsQuery & {
+      collections: Array<{ handle: string; title: string }>;
     }
-    let textStyle = {};
-    if (enableBackground) {
-        textStyle = {
-            color: '#fff',
-        };
-    } else {
-        textStyle = {
-            color: '#333',
-            position: 'relative',
-        };
-    }
-    let overlayStyle = {};
-    if (enableOverlay && enableBackground) {
-        overlayStyle = {
-            backgroundColor: `rgba(0, 0, 0, ${overlayBackground})`,
-        };
-    }
-    return (
-        <section ref={ref} {...rest} style={{ ...backgroundStyle }} className={`flex items-center justify-start relative overflow-hidden bg-center bg-no-repeat bg-cover h-[var(--header-height-mobile)] sm:h-[var(--header-height-desktop)]`}>
-            {enableOverlay && enableBackground && <div className="absolute inset-0 z-1" style={overlayStyle}></div>}
-            <div className={`text-center mx-auto w-5/6 text-gray-700 ml-auto absolute ${textPosition} z-2`} style={textStyle}>
-                <h1 className='text-4xl tracking-tight font-worksans font-normal leading-tight md:text-5xl'>
-                    {collection?.title}
-                </h1>
-                <p className="mt-4 dark:text-gray-400 font-worksans text-base md:text-sm">
-                    {collection?.description}
-                </p>
-            </div>
-        </section>
-    );
+  >();
+  let backgroundStyle: CSSProperties = {
+    '--header-height-desktop': `${sectionHeightDesktop}px`,
+    '--header-height-mobile': `${sectionHeightMobile}px`
+  } as CSSProperties;
+
+  if (enableBackground) {
+    backgroundStyle.backgroundImage = `url('${collection?.image?.url}')`;
+  }
+  let overlayStyle: CSSProperties = {};
+  if (enableOverlay && enableBackground) {
+    overlayStyle = {
+      '--overlay-opacity': `${overlayOpacity}`,
+    } as CSSProperties;
+  }
+  let positionClass: {[key: string]: string} =  {
+    'top left': 'items-start justify-start',
+    'top right': 'items-start justify-end',
+    'top center': 'items-start justify-center',
+    'center left': 'items-center justify-start',
+    'center center': 'items-center justify-center',
+    'center right': 'items-center justify-end',
+    'bottom left': 'items-end justify-start',
+    'bottom center': 'items-end justify-center',
+    'bottom right': 'items-end justify-end',
+  };
+  return (
+    <section ref={ref} {...rest} style={backgroundStyle} className={clsx(
+      'flex relative overflow-hidden bg-center bg-no-repeat bg-cover h-[var(--header-height-mobile)] sm:h-[var(--header-height-desktop)]',
+      positionClass[contentPosition]
+    )}>
+      {enableOverlay && enableBackground && <div className="absolute inset-0 z-1 bg-black bg-opacity-[var(--overlay-opacity)]" style={overlayStyle}></div>}
+      <div className={clsx(
+        'text-center w-5/6 text-gray-700 z-2 relative',
+        enableBackground ? 'text-white' : 'text-gray-700',
+      )}>
+        <h1 className='text-4xl tracking-tight font-worksans font-normal leading-tight md:text-5xl'>
+          {collection?.title}
+        </h1>
+        {collection?.description && (
+          <p className="mt-4 dark:text-gray-400 font-worksans text-base md:text-sm">
+            {collection.description}
+          </p>
+        )}
+      </div>
+    </section>
+  );
 });
 
 export default CollectHeader;
 
 export let schema: HydrogenComponentSchema = {
-    type: 'header',
-    title: 'Collection header',
-    toolbar: ['general-settings', ['duplicate', 'delete']],
-    inspector: [
+  type: 'collection-header',
+  title: 'Collection header',
+  toolbar: ['general-settings', ['duplicate', 'delete']],
+  inspector: [
+    {
+      group: 'Header',
+      inputs: [
         {
-            group: 'Header',
-            inputs: [
-                {
-                    type: 'switch',
-                    name: 'enableBackground',
-                    label: 'Enable background',
-                    defaultValue: true,
-                },
-                {
-                    type: 'range',
-                    name: 'heightSectionDesktop',
-                    label: 'Section height desktop',
-                    defaultValue: 450,
-                    configs: {
-                        min: 350,
-                        max: 550,
-                        step: 10
-                    },
-                },
-                {
-                    type: 'range',
-                    name: 'heightSectionMobile',
-                    label: 'Section height mobile',
-                    defaultValue: 450,
-                    configs: {
-                        min: 350,
-                        max: 550,
-                        step: 10
-                    },
-                },
-                {
-                    type: 'select',
-                    name: 'textPosition',
-                    label: 'Text position',
-                    configs: {
-                        options: [
-                            { value: 'top-0 right-0 left-0', label: 'Top center' },
-                            { value: 'right-0 left-0', label: 'Center' },
-                            { value: 'bottom-0 right-0 left-0', label: 'Bottom center' },
-                            { value: 'left-0', label: 'Center left' },
-                            { value: 'right-0', label: 'Center right' },
-                            { value: 'top-0 left-0', label: 'Top left' },
-                            { value: 'top-0 right-0', label: 'Top right' },
-                            { value: 'bottom-0 right-0', label: 'Bottom right' },
-                            { value: 'bottom-0 left-0', label: 'Bottom left' },
-                        ],
-                    },
-                    defaultValue: 'right-0 left-0',
-                },
-                {
-                    type: 'switch',
-                    name: 'enableOverlay',
-                    label: 'Enable overlay',
-                    defaultValue: true,
-                },
-                {
-                    type: 'range',
-                    name: 'overlayBackground',
-                    label: 'Overlay',
-                    defaultValue: 0.5,
-                    configs: {
-                        min: 0.1,
-                        max: 1,
-                        step: 0.1
-                    },
-                },
-            ],
+          type: 'switch',
+          name: 'enableBackground',
+          label: 'Enable background',
+          defaultValue: true,
         },
-    ],
+        {
+          type: 'range',
+          name: 'sectionHeightDesktop',
+          label: 'Section height desktop',
+          defaultValue: 450,
+          configs: {
+            min: 350,
+            max: 550,
+            step: 10
+          },
+        },
+        {
+          type: 'range',
+          name: 'sectionHeightMobile',
+          label: 'Section height mobile',
+          defaultValue: 450,
+          configs: {
+            min: 350,
+            max: 550,
+            step: 10
+          },
+        },
+        {
+          type: 'switch',
+          name: 'enableOverlay',
+          label: 'Enable overlay',
+          defaultValue: true,
+        },
+        {
+          type: 'range',
+          name: 'overlayOpacity',
+          label: 'Overlay opacity',
+          defaultValue: 0.5,
+          configs: {
+            min: 0.1,
+            max: 1,
+            step: 0.1
+          },
+          condition: `enableOverlay.eq.true`
+        },
+        {
+          type: 'position',
+          name: 'contentPosition',
+          label: 'Content position',
+          defaultValue: 'center center'
+        }
+      ],
+    },
+  ],
 };
