3e91b215cd96a7efd5bfcfa1cefb1ee492c5c8e9
dangthang1903 <dangthangkk2000@gmail.com>
2023-10-04 17:03:23 +0700

    feat: build section header image

---
+++ app/sections/header-image/description-text-item.tsx
@@ -0,0 +1,39 @@
+import type {
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
+} from '@weaverse/hydrogen';
+import { forwardRef } from 'react';
+
+
+interface DescriptionTextItemProps extends HydrogenComponentProps {
+  descriptionText: string;
+}
+
+let DescriptionTextItem = forwardRef<HTMLDivElement, DescriptionTextItemProps>((props, ref) => {
+  let { descriptionText, ...rest } = props;
+  return (
+    <div ref={ref} {...rest}>
+        <p className='font-sans mb-5 text-base font-normal leading-6' dangerouslySetInnerHTML={{ __html: descriptionText }}></p>
+    </div>
+  );
+});
+
+export default DescriptionTextItem;
+
+export let schema: HydrogenComponentSchema = {
+  type: 'description-text--item',
+  title: 'Descripttion text item',
+  inspector: [
+    {
+      group: 'Description text',
+      inputs: [
+        {
+          type: 'text-editor',
+          label: 'Text',
+          name: 'descriptionText',
+          defaultValue: 'Pair large text with an image to tell a story.',
+        },
+      ],
+    },
+  ],
+}
