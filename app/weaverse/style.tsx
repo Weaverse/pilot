2b7cc4d9deaf0ed11cb75863cf8299dbc2e2b445
ken <viethungpro97@gmail.com>
2023-10-05 14:58:37 +0700

    chore: update style guide

--- app/weaverse/style.tsx
+++ app/weaverse/style.tsx
@@ -10,56 +10,121 @@ function hexToRgbString(hexColor: string = ''): string {
 
 export function GlobalStyle() {
   let settings = useThemeSettings();
+  let defaultSettings = {
+    colorBackground: '#ffffff',
+    colorBackgroundSubtle: '#0f0f0f08',
+    colorText: '#0f0f0f',
+    colorTextSubtle: '#666666',
+    colorButton: '#0f0f0f',
+    colorButtonText: '#ffffff',
+    colorInverseButton: '#ffffff',
+    colorInverseButtonText: '#0f0f0f',
+    colorSale: '#de4b4b',
+    colorBorder: '#0f0f0f',
+    bodyBaseSize: 18,
+    bodyBaseSpacing: 0,
+    bodyBaseLineheight: 1.5,
+    headingBaseSize: 38,
+  };
 
   if (settings) {
+    settings = {...defaultSettings, ...settings};
     let {
-      colorPrimary,
-      colorContrast,
-      colorAccent,
-      colorPrimaryDark,
-      colorContrastDark,
-      colorAccentDark,
+      colorBackground,
+      colorBackgroundSubtle,
+      colorText,
+      colorTextSubtle,
+      colorButton,
+      colorButtonText,
+      colorInverseButton,
+      colorInverseButtonText,
+      colorSale,
+      colorBorder,
+      bodyBaseSize,
+      bodyBaseSpacing,
+      bodyBaseLineheight,
+      headingBaseSize,
+      navHeightDesktop,
+      navHeightTablet
     } = settings;
-    colorPrimary = hexToRgbString(colorPrimary?.toString());
-    colorContrast = hexToRgbString(colorContrast?.toString());
-    colorAccent = hexToRgbString(colorAccent?.toString());
-    colorPrimaryDark = hexToRgbString(colorPrimaryDark?.toString());
-    colorContrastDark = hexToRgbString(colorContrastDark?.toString());
-    colorAccentDark = hexToRgbString(colorAccentDark?.toString());
+    colorBackground = hexToRgbString(colorBackground?.toString());
+    colorBackgroundSubtle = hexToRgbString(colorBackgroundSubtle?.toString());
+    colorText = hexToRgbString(colorText?.toString());
+    colorTextSubtle = hexToRgbString(colorTextSubtle?.toString());
+    colorButton = hexToRgbString(colorButton?.toString());
+    colorButtonText = hexToRgbString(colorButtonText?.toString());
+    colorInverseButton = hexToRgbString(colorInverseButton?.toString());
+    colorInverseButtonText = hexToRgbString(colorInverseButtonText?.toString());
+    colorSale = hexToRgbString(colorSale?.toString());
+    colorBorder = hexToRgbString(colorBorder?.toString());
 
     return (
       <style
         id="global-theme-style"
         dangerouslySetInnerHTML={{
           __html: `
-            body {
-              --color-primary: ${colorPrimary}; /* Text, buttons, etc. */
-              --color-contrast: ${colorContrast}; /* Backgrounds, borders, etc. */
-              --color-accent: ${colorAccent}; /* Labels like "On sale" */
-              --font-size-display: ${settings.fontSizeDisplayMobile}rem; /* text-4xl */
-              --font-size-heading: ${settings.fontSizeHeadingMobile}rem; /* text-2xl */
-              --font-size-lead: ${settings.fontSizeLead}rem; /* text-lg */
-              --font-size-copy: ${settings.fontSizeCopy}rem; /* text-base */
-              --font-size-fine: ${settings.fontSizeFine}rem; /* text-xs */
+            :root {
+              --color-background: ${colorBackground};
+              --color-background-subtle: ${colorBackgroundSubtle};
+              --color-text: ${colorText};
+              --color-text-subtle: ${colorTextSubtle};
+              --color-button: ${colorButton};
+              --color-button-text: ${colorButtonText};
+              --color-inverse-button: ${colorInverseButton};
+              --color-inverse-button-text: ${colorInverseButtonText};
+              --color-sale: ${colorSale};
+              --color-border: ${colorBorder};
+
+              --body-base-size: ${bodyBaseSize}px;
+              --body-base-spacing: ${bodyBaseSpacing}px;
+              --body-base-lineheight: ${bodyBaseLineheight};
+              --heading-base-size: ${headingBaseSize}px;
               --height-nav: ${settings.navHeightMobile}rem;
+            }
+            body, button, input, select, textarea {
+              -webkit-font-smoothing: antialiased;
+              -webkit-text-size-adjust: 100%;
+              font-size: calc(var(--body-base-size)*0.92);
+              letter-spacing: var(--body-base-spacing);
+              line-height: var(--body-base-lineheight);
+              text-rendering: optimizeSpeed;
+            }
+
+            /* Mobile sizes */
+            h1, .h1 {
+              font-size: calc(var(--heading-base-size) * 0.85);
+            }
+
+            h2, .h2 {
+              font-size: calc(var(--heading-base-size) * 0.73);
+            }
+
+            h3, .h3 {
+              font-size: calc(var(--heading-base-size) * 0.62);
+            }
+
+            /* Desktop sizes */
+            @media (min-width: 768px) {
+              h1, .h1 {
+                font-size: var(--heading-base-size);
+              }
+
+              h2, .h2 {
+                font-size: calc(var(--heading-base-size) * 0.85);
+              }
+
+              h3, .h3 {
+                font-size: calc(var(--heading-base-size) * 0.65);
+              }
             }
             @media (min-width: 32em) {
               body {
-                --height-nav: ${settings.navHeightTablet}rem;
+                --height-nav: ${navHeightTablet}rem;
               }
             }
             @media (min-width: 48em) {
               body {
-                --height-nav: ${settings.navHeightDesktop}rem;
-                --font-size-display: ${settings.fontSizeDisplayDesktop}rem; /* text-6xl */
-                --font-size-heading: ${settings.fontSizeHeadingDesktop}rem; /* text-4xl */
-              }
-            }
-            @media (prefers-color-scheme: dark) {
-              body {
-                --color-primary: ${colorPrimaryDark};
-                --color-contrast: ${colorContrastDark};
-                --color-accent: ${colorAccentDark};
+                --height-nav: ${navHeightDesktop}rem;
               }
             }
           `,
