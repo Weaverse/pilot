4972e66641afbef5159bfa3b14eab9c79f19c54b
ken <viethungpro97@gmail.com>
2023-12-08 10:57:10 +0700

    chore: optimize variant swatch

--- app/sections/single-product/options.tsx
+++ app/sections/single-product/options.tsx
@@ -1,13 +1,16 @@
 import clsx from 'clsx';
 import {Image} from '@shopify/hydrogen';
+import { WeaverseImage } from '@weaverse/hydrogen';
 interface VariantOptionProps {
   selectedOptionValue: string;
   onSelectOptionValue: (optionValue: string) => void;
   name: string;
-  size?: 'sm' | 'md' | 'lg';
-  type?: string;
-  shape?: string;
-  displayName?: string;
+  config: {
+    type: string;
+    displayName: string;
+    size?: 'sm' | 'md' | 'lg';
+    shape?: string;
+  };
   swatches: {
     imageSwatches: any[];
     colorSwatches: any[];
@@ -37,15 +40,14 @@ let BUTTON_SIZE_MAP = {
 export function VariantOption(props: VariantOptionProps) {
   let {
     name,
-    type = 'default',
     values,
-    displayName,
-    shape = 'square',
-    size = 'md',
     selectedOptionValue,
     onSelectOptionValue,
     swatches,
+    config,
   } = props;
+
+  let {displayName, shape = 'square', size = 'md', type = 'default'} = config;
 
   let roundedClassName =
     shape === 'circle' ? 'rounded-full' : shape === 'round' ? 'rounded-md' : '';
@@ -122,7 +124,7 @@ export function VariantOption(props: VariantOptionProps) {
       {type === 'custom-image' && (
         <div className="flex gap-4">
           {values.map((value) => {
-            let swatchImage: string =
+            let swatchImage: WeaverseImage =
               swatches.imageSwatches.find((image) => image.name === value.value)
                 ?.value || '';
             return (
