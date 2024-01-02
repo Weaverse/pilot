e5c70c99bd4d8d4fa9c8d762ac00c38346a4fef9
hta218 <huynhtuananh21895@gmail.com>
2024-01-02 14:12:22 +0700

    Update component imports in components.ts

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -1,16 +1,12 @@
 import type { HydrogenComponent } from '@weaverse/hydrogen';
-import * as RichText from '~/sections/RichText/RichText';
-import * as VideoItem from '~/sections/Video/VideoItem';
-import * as Video from '~/sections/Video/video';
-import * as VideoWithText from '~/sections/VideoWithText/VideoWithText';
 import * as AllProducts from '~/sections/all-products';
 import * as BlogPost from '~/sections/blog-post';
 import * as Blogs from '~/sections/blogs';
+import * as CollectionBanner from '~/sections/collection-banner';
 import * as CollectionFilters from '~/sections/collection-filters';
-import * as CollectionHeader from '~/sections/collection-header';
 import * as CollectionList from '~/sections/collection-list';
-import * as ContentColumnWithImage from '~/sections/column-with-image/index';
-import * as ContentColumnItem from '~/sections/column-with-image/item';
+import * as ColumnWithImage from '~/sections/column-with-image';
+import * as ColumnWithImageItem from '~/sections/column-with-image/item';
 import * as Countdown from '~/sections/countdown';
 import * as CountdownActions from '~/sections/countdown/actions';
 import * as CountDownTimer from '~/sections/countdown/timer';
@@ -24,7 +20,6 @@ import * as ImageGalleryItems from '~/sections/image-gallery/items';
 import * as ImageWithText from '~/sections/image-with-text';
 import * as ImageWithTextContent from '~/sections/image-with-text/content';
 import * as ImageWithTextImage from '~/sections/image-with-text/image';
-import * as Judgeme from '~/sections/judgeme-review';
 import * as Map from '~/sections/map';
 import * as Page from '~/sections/page';
 import * as ProductInformation from '~/sections/product-information';
@@ -35,25 +30,31 @@ import * as RelatedArticles from '~/sections/related-articles';
 import * as RelatedProducts from '~/sections/related-products';
 import { commonComponents } from '~/sections/shared/atoms';
 import * as SingleProduct from '~/sections/single-product';
+import * as Judgeme from '~/sections/single-product/judgeme-review';
 import * as Testimonial from '~/sections/testimonials';
 import * as TestimonialItem from '~/sections/testimonials/item';
 import * as TestimonialItems from '~/sections/testimonials/items';
+import * as VideoBanner from '~/sections/video-banner';
+import * as VideoEmbed from '~/sections/video-embed';
+import * as VideoEmbedItem from '~/sections/video-embed/video';
 
 export let components: HydrogenComponent[] = [
   ...commonComponents,
+  AllProducts,
+  BlogPost,
+  Blogs,
+  CollectionBanner,
   Hero,
   Page,
-  Video,
-  VideoItem,
-  CollectionHeader,
+  VideoEmbed,
+  VideoEmbedItem,
   ImageBanner,
   ImageWithText,
   ImageWithTextContent,
   ImageWithTextImage,
-  ContentColumnWithImage,
-  ContentColumnItem,
-  RichText,
-  VideoWithText,
+  ColumnWithImage,
+  ColumnWithImageItem,
+  VideoBanner,
   Map,
   PromotionGrid,
   PromotionGridItem,
@@ -61,9 +62,6 @@ export let components: HydrogenComponent[] = [
   Countdown,
   CountDownTimer,
   CountdownActions,
-  Blogs,
-  BlogPost,
-  AllProducts,
   FeaturedProducts,
   FeaturedCollections,
   Testimonial,
