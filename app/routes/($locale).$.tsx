8ff4cef3bc041b1340b355db97cddffe84a0d9e4
Paul <phanminh65@gmail.com>
2024-02-02 19:10:22 +0700

    chore: remove logs

--- app/routes/($locale).$.tsx
+++ app/routes/($locale).$.tsx
@@ -1,7 +1,6 @@
 import {WeaverseContent} from '~/weaverse';
 import {type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 export async function loader({context}: LoaderFunctionArgs) {
-  console.log('context', context);
   let weaverseData = await context.weaverse.loadPage({
     type: 'CUSTOM',
   });
--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -13,7 +13,6 @@ export async function loader(args: LoaderFunctionArgs) {
   let {params, context} = args;
   let {language, country} = context.storefront.i18n;
 
-  console.log(111, params, context, language, country);
   let pageType: PageType = 'INDEX';
   if (
     params.locale &&
