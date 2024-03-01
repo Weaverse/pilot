a7df9623a171e224297f8a625d18adbe32a358c3
Paul <phanminh65@gmail.com>
2024-03-01 18:13:55 +0700

    Handle not found route

--- app/routes/($locale).$.tsx
+++ app/routes/($locale).$.tsx
@@ -1,16 +1,17 @@
-import {WeaverseContent} from '~/weaverse';
-import {type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import { WeaverseContent } from '~/weaverse';
+import { type LoaderFunctionArgs } from '@shopify/remix-oxygen';
 
-export async function loader({context}: LoaderFunctionArgs) {
+export async function loader({ context }: LoaderFunctionArgs) {
   let weaverseData = await context.weaverse.loadPage({
     type: 'CUSTOM',
   });
-  if (!weaverseData) {
-    return new Response(null, {status: 404});
+
+  if (weaverseData?.page?.id && !weaverseData.page.id.includes('fallback')) {
+    return {
+      weaverseData,
+    };
   }
-  return {
-    weaverseData,
-  };
+  throw new Response(null, { status: 404 });
 }
 
 export default function Component() {
