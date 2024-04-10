454ccbb7aafa2dbe7a7525893de1ed227a39c671
Paul <phanminh65@gmail.com>
2024-04-10 22:36:54 +0700

    handle 404 page

--- app/routes/($locale).$.tsx
+++ app/routes/($locale).$.tsx
@@ -1,18 +1,18 @@
-import {WeaverseContent} from '~/weaverse';
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
-import type {LoadPageParams} from '@weaverse/hydrogen';
+import { WeaverseContent } from '~/weaverse';
+import type { LoaderFunctionArgs } from '@shopify/remix-oxygen';
 
-export async function loader({context}: LoaderFunctionArgs) {
+export async function loader({ context }: LoaderFunctionArgs) {
   let weaverseData = await context.weaverse.loadPage({
     type: 'CUSTOM',
   });
+
   if (weaverseData?.page?.id && !weaverseData.page.id.includes('fallback')) {
     return {
       weaverseData,
     };
   }
   // If Weaverse Data not found, return 404
-  throw new Response(null, {status: 404});
+  throw new Response(null, { status: 404 });
 }
 
 export default function Component() {
--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -1,31 +1,33 @@
-import {AnalyticsPageType} from '@shopify/hydrogen';
-import {defer, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
-import {routeHeaders} from '~/data/cache';
-import {SHOP_QUERY} from '~/data/queries';
-import {seoPayload} from '~/lib/seo.server';
-import {WeaverseContent} from '~/weaverse';
-import type {LoadPageParams} from '@weaverse/hydrogen';
+import { AnalyticsPageType } from '@shopify/hydrogen';
+import { defer, type LoaderFunctionArgs } from '@shopify/remix-oxygen';
+import { routeHeaders } from '~/data/cache';
+import { SHOP_QUERY } from '~/data/queries';
+import { seoPayload } from '~/lib/seo.server';
+import { WeaverseContent } from '~/weaverse';
+import type { PageType } from '@weaverse/hydrogen';
 export const headers = routeHeaders;
 
 export async function loader(args: LoaderFunctionArgs) {
-  let {params, context} = args;
-  let {pathPrefix} = context.storefront.i18n;
+  let { params, context } = args;
+  let { pathPrefix } = context.storefront.i18n;
   let locale = pathPrefix.slice(1);
-  let weaverseQuery: LoadPageParams = {
-    type: 'INDEX',
-  };
+  let type: PageType = 'INDEX';
 
   if (params.locale && params.locale.toLowerCase() !== locale) {
     // Update for Weaverse: if it not locale, it probably is a custom page handle
-    weaverseQuery.type = 'CUSTOM';
+    type = 'CUSTOM';
+  }
+  let weaverseData = await context.weaverse.loadPage({ type })
+  if (!weaverseData?.page?.id || weaverseData.page.id.includes('fallback')) {
+    throw new Response(null, { status: 404 });
   }
 
-  let {shop} = await context.storefront.query(SHOP_QUERY);
+  let { shop } = await context.storefront.query(SHOP_QUERY);
   let seo = seoPayload.home();
 
   return defer({
     shop,
-    weaverseData: await context.weaverse.loadPage(weaverseQuery),
+    weaverseData,
     analytics: {
       pageType: AnalyticsPageType.home,
     },
