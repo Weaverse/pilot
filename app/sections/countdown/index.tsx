7b0efc20dc2ed399b3368442a86d1fc4a598812b
hta218 <huynhtuananh21895@gmail.com>
2024-05-10 08:03:59 +0700

    Remove unused `CountdownActions` component

--- app/sections/countdown/index.tsx
+++ app/sections/countdown/index.tsx
@@ -1,34 +1,11 @@
-import type {
-  HydrogenComponentProps,
-  HydrogenComponentSchema,
-  WeaverseImage,
-} from "@weaverse/hydrogen";
-import type { CSSProperties } from "react";
+import type { HydrogenComponentSchema } from "@weaverse/hydrogen";
 import { forwardRef } from "react";
-import { Image } from "@shopify/hydrogen";
-import { Section, SectionProps } from "../shared/Section";
+import { Section, SectionProps, sectionInspector } from "../shared/Section";
 
-type CountdownData = {
-  backgroundColor: string;
-  backgroundImage: WeaverseImage;
-  overlayColor: string;
-  overlayOpacity: number;
-  sectionHeight: number;
-};
-
-type CountdownProps = SectionProps & CountdownData;
+type CountdownProps = SectionProps;
 
 let Countdown = forwardRef<HTMLElement, CountdownProps>((props, ref) => {
-  let {
-    backgroundColor,
-    backgroundImage,
-    overlayColor,
-    overlayOpacity,
-    sectionHeight,
-    children,
-    ...rest
-  } = props;
-
+  let { children, ...rest } = props;
   return (
     <Section ref={ref} {...rest}>
       {children}
@@ -42,59 +19,8 @@ export let schema: HydrogenComponentSchema = {
   type: "countdown",
   title: "Countdown",
   toolbar: ["general-settings", ["duplicate", "delete"]],
-  inspector: [
-    {
-      group: "Countdown",
-      inputs: [
-        {
-          type: "color",
-          name: "backgroundColor",
-          label: "Background color",
-          defaultValue: "#ffffff",
-        },
-        {
-          type: "image",
-          name: "backgroundImage",
-          label: "Background image",
-        },
-        {
-          type: "color",
-          name: "overlayColor",
-          label: "Overlay color",
-        },
-        {
-          type: "range",
-          name: "overlayOpacity",
-          label: "Overlay opacity",
-          defaultValue: 50,
-          configs: {
-            min: 10,
-            max: 100,
-            step: 10,
-            unit: "%",
-          },
-        },
-        {
-          type: "range",
-          name: "sectionHeight",
-          label: "Section height",
-          defaultValue: 450,
-          configs: {
-            min: 400,
-            max: 700,
-            step: 10,
-            unit: "px",
-          },
-        },
-      ],
-    },
-  ],
-  childTypes: [
-    "heading",
-    "subheading",
-    "countdown--timer",
-    "countdown--actions",
-  ],
+  inspector: [sectionInspector],
+  childTypes: ["heading", "subheading", "countdown--timer", "button"],
   presets: {
     children: [
       {
@@ -109,7 +35,7 @@ export let schema: HydrogenComponentSchema = {
         type: "countdown--timer",
       },
       {
-        type: "countdown--actions",
+        type: "button",
       },
     ],
   },
