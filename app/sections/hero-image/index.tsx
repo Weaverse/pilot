c6504b7e1a8b6cd307f5b3bac0a86c7517ed5fcc
hta218 <huynhtuananh21895@gmail.com>
2024-05-25 16:30:27 +0700

    Add `HeroImage` section

--- app/sections/hero-image/index.tsx
+++ app/sections/hero-image/index.tsx
@@ -1,31 +1,67 @@
-import type { HydrogenComponentSchema } from "@weaverse/hydrogen";
-import type { CSSProperties } from "react";
+import {
+  IMAGES_PLACEHOLDERS,
+  type HydrogenComponentSchema,
+} from "@weaverse/hydrogen";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+import clsx from "clsx";
 import { forwardRef } from "react";
+import { backgroundInputs } from "~/components/BackgroundImage";
+import { overlayInputs } from "~/components/Overlay";
 import type { SectionProps } from "~/components/Section";
-import { Section, sectionInspector } from "~/components/Section";
+import { Section, layoutInputs } from "~/components/Section";
 
-type HeroImageProps = SectionProps & {
-  sectionHeightDesktop: number;
-  sectionHeightMobile: number;
-};
+export interface HeroImageProps extends VariantProps<typeof variants> {}
 
-let HeroImage = forwardRef<HTMLElement, HeroImageProps>((props, ref) => {
-  let { backgroundImage, children, ...rest } = props;
-  let sectionStyle: CSSProperties = {
-    justifyContent: `${contentAlignment}`,
-    "--section-height-desktop": `${sectionHeightDesktop}px`,
-    "--section-height-mobile": `${sectionHeightMobile}px`,
-    "--overlay-opacity": `${overlayOpacity}%`,
-    "--overlay-color": `${overlayColor}`,
-    "--max-width-content": "600px",
-  } as CSSProperties;
-
-  return (
-    <Section ref={ref} {...rest} style={sectionStyle}>
-      {children}
-    </Section>
-  );
+let variants = cva("", {
+  variants: {
+    height: {
+      small: "min-h-[375px] sm:min-h-[400px] lg:min-h-[440px]",
+      medium: "min-h-[480px] sm:min-h-[460px] lg:min-h-[500px]",
+      large: "min-h-[560px] sm:min-h-[560px] lg:min-h-[640px]",
+      full: "h-screen-no-nav",
+    },
+    contentPosition: {
+      "top left": "justify-start items-start [&_.paragraph]:[text-align:left]",
+      "top center":
+        "justify-start items-center [&_.paragraph]:[text-align:center]",
+      "top right": "justify-start items-end [&_.paragraph]:[text-align:right]",
+      "center left":
+        "justify-center items-start [&_.paragraph]:[text-align:left]",
+      "center center":
+        "justify-center items-center [&_.paragraph]:[text-align:center]",
+      "center right":
+        "justify-center items-end [&_.paragraph]:[text-align:right]",
+      "bottom left": "justify-end items-start [&_.paragraph]:[text-align:left]",
+      "bottom center":
+        "justify-end items-center [&_.paragraph]:[text-align:center]",
+      "bottom right": "justify-end items-end [&_.paragraph]:[text-align:right]",
+    },
+  },
+  defaultVariants: {
+    height: "large",
+    contentPosition: "center center",
+  },
 });
+
+let HeroImage = forwardRef<HTMLElement, HeroImageProps & SectionProps>(
+  (props, ref) => {
+    let { children, height, contentPosition, ...rest } = props;
+    return (
+      <Section
+        ref={ref}
+        {...rest}
+        containerClassName={clsx(
+          "flex flex-col",
+          "[&_.paragraph]:mx-[unset]",
+          variants({ contentPosition, height }),
+        )}
+      >
+        {children}
+      </Section>
+    );
+  },
+);
 
 export default HeroImage;
 
@@ -33,22 +69,72 @@ export let schema: HydrogenComponentSchema = {
   type: "hero-image",
   title: "Hero image",
   toolbar: ["general-settings", ["duplicate", "delete"]],
-  inspector: [sectionInspector],
+  inspector: [
+    {
+      group: "Layout",
+      inputs: [
+        {
+          type: "select",
+          name: "height",
+          label: "Section height",
+          configs: {
+            options: [
+              { value: "small", label: "Small" },
+              { value: "medium", label: "Medium" },
+              { value: "large", label: "Large" },
+              { value: "full", label: "Fullscreen" },
+            ],
+          },
+        },
+        {
+          type: "position",
+          name: "contentPosition",
+          label: "Content position",
+          defaultValue: "center center",
+        },
+        ...layoutInputs.filter(
+          (inp) => inp.name !== "divider" && inp.name !== "borderRadius",
+        ),
+      ],
+    },
+    {
+      group: "Background",
+      inputs: [
+        ...backgroundInputs.filter(
+          (inp) =>
+            inp.name !== "backgroundFor" && inp.name !== "backgroundColor",
+        ),
+      ],
+    },
+    { group: "Overlay", inputs: [...overlayInputs] },
+  ],
   childTypes: ["subheading", "heading", "description", "button"],
   presets: {
+    height: "large",
+    contentPosition: "bottom left",
+    backgroundImage: IMAGES_PLACEHOLDERS.backgroundImage,
+    backgroundFit: "cover",
+    enableOverlay: true,
+    overlayOpacity: 35,
     children: [
       {
         type: "subheading",
         content: "Subheading",
+        color: "#ffffff",
       },
       {
         type: "heading",
         content: "Hero image with text overlay",
+        color: "#ffffff",
+        size: "scale",
+        minSize: 16,
+        maxSize: 56,
       },
       {
         type: "description",
         content:
           "Use this text to share information about your brand with your customers. Describe a product, share announcements, or welcome customers to your store.",
+        color: "#ffffff",
       },
     ],
   },
