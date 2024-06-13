5c4651266fc77856df60108dec1c9faa8fc9ab9b
ken <viethungpro97@gmail.com>
2024-06-13 17:36:32 +0700

    chore: revert icon, remove unused code

--- app/modules/Layout.tsx
+++ app/modules/Layout.tsx
@@ -150,36 +150,6 @@ export function MenuDrawer({
   );
 }
 
-// function MenuMobileNav({
-//   menu,
-//   onClose,
-// }: {
-//   menu: EnhancedMenu;
-//   onClose: () => void;
-// }) {
-//   return (
-//     <nav className="grid gap-4 p-6 sm:gap-6 sm:px-12 sm:py-8">
-//       {/* Top level menu items */}
-//       {(menu?.items || []).map((item) => (
-//         <span key={item.id} className="block">
-//           <Link
-//             to={item.to}
-//             target={item.target}
-//             onClick={onClose}
-//             className={({ isActive }) =>
-//               isActive ? "pb-1 border-b -mb-px" : "pb-1"
-//             }
-//           >
-//             <Text as="span" size="copy">
-//               {item.title}
-//             </Text>
-//           </Link>
-//         </span>
-//       ))}
-//     </nav>
-//   );
-// }
-
 function MobileHeader({
   title,
   isHome,
