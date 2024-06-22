04f5cad4fbfb97b779e6494055f33898b28332a7
hta218 <huynhtuananh21895@gmail.com>
2024-06-22 15:21:25 +0700

    Add OverlayAndBackground component

---
+++ app/components/OverlayAndBackground.tsx
@@ -0,0 +1,33 @@
+import type { BackgroundImageProps } from "./BackgroundImage";
+import { BackgroundImage } from "./BackgroundImage";
+import type { OverlayProps } from "./Overlay";
+import { Overlay } from "./Overlay";
+
+export interface OverlayAndBackgroundProps
+  extends BackgroundImageProps,
+    OverlayProps {}
+
+export function OverlayAndBackground(props: OverlayAndBackgroundProps) {
+  let {
+    backgroundImage,
+    backgroundFit,
+    backgroundPosition,
+    enableOverlay,
+    overlayColor,
+    overlayOpacity,
+  } = props;
+  return (
+    <>
+      <BackgroundImage
+        backgroundImage={backgroundImage}
+        backgroundFit={backgroundFit}
+        backgroundPosition={backgroundPosition}
+      />
+      <Overlay
+        enableOverlay={enableOverlay}
+        overlayColor={overlayColor}
+        overlayOpacity={overlayOpacity}
+      />
+    </>
+  );
+}
