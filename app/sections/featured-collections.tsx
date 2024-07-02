07331337fd7f6d8f5ddd70566e254179d1b7accd
hta218 <huynhtuananh21895@gmail.com>
2024-07-02 11:36:57 +0700

    Add `CollectionList` section

--- app/sections/featured-collections.tsx
+++ app/sections/featured-collections.tsx
@@ -1,79 +1,103 @@
 import type {
-  HydrogenComponentProps,
-  HydrogenComponentSchema,
   ComponentLoaderArgs,
+  HydrogenComponentSchema,
+  WeaverseCollection,
 } from "@weaverse/hydrogen";
 import { forwardRef } from "react";
-
-import type { HomepageFeaturedCollectionsQuery } from "storefrontapi.generated";
-import { FeaturedCollections as HomeFeaturedCollections } from "~/modules";
-import { FEATURED_COLLECTIONS_QUERY } from "~/data/queries";
-
-interface FeaturedCollectionsProps
-  extends HydrogenComponentProps<Awaited<ReturnType<typeof loader>>> {
-  heading: string;
-  collectionsCount: number;
+import type { CollectionsByIdsQuery } from "storefrontapi.generated";
+import type { SectionProps } from "~/components/Section";
+import { Section, layoutInputs } from "~/components/Section";
+interface CollectionListData {
+  collections: WeaverseCollection[];
 }
 
-let FeaturedCollections = forwardRef<HTMLElement, FeaturedCollectionsProps>(
+interface CollectionListProps
+  extends SectionProps<Awaited<ReturnType<typeof loader>>>,
+    CollectionListData {}
+
+let CollectionList = forwardRef<HTMLElement, CollectionListProps>(
   (props, ref) => {
-    let { loaderData, heading, collectionsCount, ...rest } = props;
+    let { loaderData, ...rest } = props;
     return (
-      <section ref={ref} {...rest}>
-        {loaderData?.collections?.nodes ? (
+      <Section ref={ref} {...rest}>
+        collections list
+        {/* {loaderData?.collections?.nodes ? (
           <HomeFeaturedCollections
             collections={loaderData.collections}
-            count={collectionsCount}
+            count={10}
             title={heading}
           />
-        ) : null}
-      </section>
+        ) : null} */}
+      </Section>
     );
   },
 );
 
-export default FeaturedCollections;
+export default CollectionList;
 
-export let loader = async ({ weaverse }: ComponentLoaderArgs) => {
+let COLLECTIONS_QUERY = `#graphql
+  query collectionsByIds($country: CountryCode, $language: LanguageCode, $ids: [ID!]!)
+  @inContext(country: $country, language: $language) {
+    nodes(ids: $ids) {
+      ... on Collection {
+        id
+        title
+        handle
+        onlineStoreUrl
+        description
+        image {
+          id
+          altText
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+` as const;
+
+export let loader = async ({
+  data,
+  weaverse,
+}: ComponentLoaderArgs<CollectionListData>) => {
   let { language, country } = weaverse.storefront.i18n;
-  return await weaverse.storefront.query<HomepageFeaturedCollectionsQuery>(
-    FEATURED_COLLECTIONS_QUERY,
-    {
-      variables: {
-        country,
-        language,
-      },
-    },
+  let ids = data.collections.map(
+    (collection) => `gid://shopify/Collection/${collection.id}`,
   );
+  if (ids.length) {
+    let { nodes } = await weaverse.storefront.query<CollectionsByIdsQuery>(
+      COLLECTIONS_QUERY,
+      {
+        variables: {
+          country,
+          language,
+          ids,
+        },
+      },
+    );
+    return nodes;
+  }
+  return [];
 };
 
 export let schema: HydrogenComponentSchema = {
-  type: "featured-collections",
-  title: "Featured collections",
+  type: "collection-list",
+  title: "Collection list",
   inspector: [
     {
-      group: "Featured collections",
+      group: "Collection list",
       inputs: [
         {
-          type: "text",
-          name: "heading",
-          label: "Heading",
-          defaultValue: "Featured Collections",
-          placeholder: "Featured Collections",
-        },
-        {
-          type: "range",
-          name: "collectionsCount",
-          label: "Number of collections",
-          defaultValue: 4,
-          configs: {
-            min: 1,
-            max: 4,
-            step: 1,
-          },
+          type: "collection-list",
+          name: "collections",
+          label: "Collections",
         },
       ],
     },
+    {
+      group: "Layout",
+      inputs: layoutInputs.filter((i) => i.name !== "borderRadius"),
+    },
   ],
-  toolbar: ["general-settings", ["duplicate", "delete"]],
 };
