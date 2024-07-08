3d010b01198b6635003c8bfe9407162418898d67
ken <viethungpro97@gmail.com>
2024-07-08 16:25:16 +0700

    chore: update mobile header

---
+++ app/modules/header/MobileHeader.tsx
@@ -0,0 +1,85 @@
+import { Await, Form, Link, useParams } from "@remix-run/react";
+import { Suspense } from "react";
+import { useRootLoaderData } from "~/root";
+import { IconAccount, IconLogin, IconMenu, IconSearch } from "../Icon";
+import { Logo } from "../Logo";
+import { CartCount } from "./CartCount";
+import useWindowScroll from "react-use/esm/useWindowScroll";
+import { useThemeSettings } from "@weaverse/hydrogen";
+import clsx from "clsx";
+
+export function MobileHeader({
+  title,
+  isHome,
+  openCart,
+  openMenu,
+}: {
+  title: string;
+  isHome: boolean;
+  openCart: () => void;
+  openMenu: () => void;
+}) {
+  // useHeaderStyleFix(containerStyle, setContainerStyle, isHome);
+  let settings = useThemeSettings();
+  const { y } = useWindowScroll();
+  let enableTransparent = settings?.enableTransparentHeader && isHome;
+  let isTransparent = enableTransparent && y < 50;
+  console.log("🚀 ~ isTransparent:", isTransparent);
+  const params = useParams();
+  return (
+    <header
+      role="banner"
+      className={clsx(
+        enableTransparent ? "fixed w-screen" : "sticky",
+        isTransparent
+          ? "bg-transparent text-primary"
+          : "shadow-header text-body bg-primary",
+        "transition-all duration-300 ease-in-out",
+        "flex lg:hidden items-center h-nav z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8",
+      )}
+    >
+      <div className="flex items-center justify-start w-full gap-4">
+        <button
+          onClick={openMenu}
+          className="relative flex items-center justify-center w-8 h-8"
+        >
+          <IconMenu />
+        </button>
+        <Form
+          method="get"
+          action={params.locale ? `/${params.locale}/search` : "/search"}
+          className="items-center gap-2 sm:flex"
+        >
+          <button
+            type="submit"
+            className="relative flex items-center justify-center w-8 h-8"
+          >
+            <IconSearch />
+          </button>
+        </Form>
+      </div>
+
+      <Logo showTransparent={isTransparent} />
+
+      <div className="flex items-center justify-end w-full gap-4">
+        <AccountLink className="relative flex items-center justify-center w-8 h-8" />
+        <CartCount isHome={isHome} openCart={openCart} />
+      </div>
+    </header>
+  );
+}
+
+function AccountLink({ className }: { className?: string }) {
+  const rootData = useRootLoaderData();
+  const isLoggedIn = rootData?.isLoggedIn;
+
+  return (
+    <Link to="/account" className={className}>
+      <Suspense fallback={<IconLogin />}>
+        <Await resolve={isLoggedIn} errorElement={<IconAccount />}>
+          {(isLoggedIn) => (isLoggedIn ? <IconAccount /> : <IconAccount />)}
+        </Await>
+      </Suspense>
+    </Link>
+  );
+}
--- app/modules/header/index.tsx
+++ app/modules/header/index.tsx
@@ -1,4 +1,4 @@
-import { Await, Form, Link, useParams } from "@remix-run/react";
+import { Await } from "@remix-run/react";
 import { CartForm } from "@shopify/hydrogen";
 import { Suspense, useEffect } from "react";
 import { useCartFetchers } from "~/hooks/useCartFetchers";
@@ -7,11 +7,9 @@ import { useRootLoaderData } from "~/root";
 import { Cart } from "../Cart";
 import { CartLoading } from "../CartLoading";
 import { Drawer, useDrawer } from "../Drawer";
-import { IconAccount, IconLogin, IconMenu, IconSearch } from "../Icon";
-import { Logo } from "../Logo";
-import { CartCount } from "./CartCount";
 import { DesktopHeader } from "./DesktopHeader";
 import { MobileMenu } from "./menu/MobileMenu";
+import { MobileHeader } from "./MobileHeader";
 
 export function Header({
   title,
@@ -106,76 +104,5 @@ export function MenuDrawer({
     >
       {<MobileMenu menu={menu} />}
     </Drawer>
-  );
-}
-
-function MobileHeader({
-  title,
-  isHome,
-  openCart,
-  openMenu,
-}: {
-  title: string;
-  isHome: boolean;
-  openCart: () => void;
-  openMenu: () => void;
-}) {
-  // useHeaderStyleFix(containerStyle, setContainerStyle, isHome);
-
-  const params = useParams();
-
-  return (
-    <header
-      role="banner"
-      className={`${
-        isHome
-          ? "bg-primary text-body shadow-darkHeader"
-          : "bg-primary text-body"
-      } flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`}
-    >
-      <div className="flex items-center justify-start w-full gap-4">
-        <button
-          onClick={openMenu}
-          className="relative flex items-center justify-center w-8 h-8"
-        >
-          <IconMenu />
-        </button>
-        <Form
-          method="get"
-          action={params.locale ? `/${params.locale}/search` : "/search"}
-          className="items-center gap-2 sm:flex"
-        >
-          <button
-            type="submit"
-            className="relative flex items-center justify-center w-8 h-8"
-          >
-            <IconSearch />
-          </button>
-        </Form>
-      </div>
-
-      <Logo />
-
-      <div className="flex items-center justify-end w-full gap-4">
-        <AccountLink className="relative flex items-center justify-center w-8 h-8" />
-        <CartCount isHome={isHome} openCart={openCart} />
-      </div>
-    </header>
-  );
-}
-
-
-function AccountLink({ className }: { className?: string }) {
-  const rootData = useRootLoaderData();
-  const isLoggedIn = rootData?.isLoggedIn;
-
-  return (
-    <Link to="/account" className={className}>
-      <Suspense fallback={<IconLogin />}>
-        <Await resolve={isLoggedIn} errorElement={<IconAccount />}>
-          {(isLoggedIn) => (isLoggedIn ? <IconAccount /> : <IconAccount />)}
-        </Await>
-      </Suspense>
-    </Link>
   );
 }
