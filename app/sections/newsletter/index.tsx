b8c4239a622de600f4335431a32d86d91b8e8267
hta218 <huynhtuananh21895@gmail.com>
2024-07-12 22:57:42 +0700

    Add `Newsletter` section

---
+++ app/sections/newsletter/index.tsx
@@ -0,0 +1,39 @@
+import type { HydrogenComponentSchema } from "@weaverse/hydrogen";
+import { forwardRef } from "react";
+import type { SectionProps } from "~/components/Section";
+import { Section, sectionInspector } from "~/components/Section";
+
+type NewsLetterProps = SectionProps;
+
+let NewsLetter = forwardRef<HTMLElement, NewsLetterProps>((props, ref) => {
+  let { children, ...rest } = props;
+  return (
+    <Section ref={ref} {...rest}>
+      {children}
+    </Section>
+  );
+});
+
+export default NewsLetter;
+
+export let schema: HydrogenComponentSchema = {
+  type: "newsletter",
+  title: "Newsletter",
+  inspector: sectionInspector,
+  childTypes: ["subheading", "heading", "paragraph", "newsletter-input"],
+  presets: {
+    gap: 20,
+    children: [
+      {
+        type: "heading",
+        content: "SIGN UP & SAVE 15%",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Subscribe to get special offers, free giveaways, and once-in-a-lifetime deals.",
+      },
+      { type: "newsletter-input" },
+    ],
+  },
+};
---
+++ app/sections/newsletter/newsletter-input.tsx
@@ -0,0 +1,137 @@
+import type {
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
+} from "@weaverse/hydrogen";
+import { forwardRef } from "react";
+import Button, {
+  type ButtonStyleProps,
+  buttonStylesInputs,
+} from "~/components/Button";
+import { Input } from "~/modules/Input";
+
+interface NewsLetterInputProps
+  extends HydrogenComponentProps,
+    ButtonStyleProps {
+  width: number;
+  placeholder: string;
+  gap: number;
+  buttonText: string;
+  buttonVariant: "link" | "primary" | "secondary";
+}
+
+let NewsLetterInput = forwardRef<HTMLDivElement, NewsLetterInputProps>(
+  (props, ref) => {
+    let {
+      buttonText,
+      buttonVariant,
+      buttonStyle,
+      backgroundColor,
+      textColor,
+      borderColor,
+      backgroundColorHover,
+      textColorHover,
+      borderColorHover,
+      width,
+      placeholder,
+      gap,
+      ...rest
+    } = props;
+    return (
+      <div
+        ref={ref}
+        {...rest}
+        style={{ gap }}
+        className="flex flex-col md:flex-row max-w-[90%] items-center w-fit mx-auto"
+      >
+        <Input
+          name="email"
+          type="email"
+          required
+          placeholder={placeholder}
+          style={{ width }}
+        />
+        <Button
+          text={buttonText}
+          variant={buttonVariant}
+          buttonStyle={buttonStyle}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+          borderColor={borderColor}
+          backgroundColorHover={backgroundColorHover}
+          textColorHover={textColorHover}
+          borderColorHover={borderColorHover}
+        />
+      </div>
+    );
+  },
+);
+
+export default NewsLetterInput;
+
+export let schema: HydrogenComponentSchema = {
+  type: "newsletter-input",
+  title: "Input",
+  inspector: [
+    {
+      group: "Input",
+      inputs: [
+        {
+          type: "range",
+          name: "gap",
+          label: "Items gap",
+          configs: {
+            min: 20,
+            max: 100,
+            step: 4,
+            unit: "px",
+          },
+          defaultValue: 20,
+        },
+        {
+          type: "range",
+          name: "width",
+          label: "Input width",
+          configs: {
+            min: 250,
+            max: 450,
+            step: 10,
+            unit: "px",
+          },
+          defaultValue: 300,
+        },
+        {
+          type: "text",
+          name: "placeholder",
+          label: "Placeholder",
+          defaultValue: "Enter your email",
+          placeholder: "Enter your email",
+        },
+        {
+          type: "heading",
+          label: "Subscribe button",
+        },
+        {
+          type: "text",
+          name: "buttonText",
+          label: "Button text",
+          placeholder: "Subscribe",
+          defaultValue: "Subscribe",
+        },
+        {
+          type: "select",
+          name: "buttonVariant",
+          label: "Variant",
+          configs: {
+            options: [
+              { label: "Primary", value: "primary" },
+              { label: "Secondary", value: "secondary" },
+              { label: "Link", value: "link" },
+            ],
+          },
+          defaultValue: "primary",
+        },
+        ...buttonStylesInputs,
+      ],
+    },
+  ],
+};
