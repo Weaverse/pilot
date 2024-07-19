3f2597912e3ddd678aa35762d5dee0caefc408da
hta218 <huynhtuananh21895@gmail.com>
2024-07-19 17:48:14 +0700

    Falling back to `shop.name` in logo component if no logo data available

--- app/modules/Logo.tsx
+++ app/modules/Logo.tsx
@@ -10,33 +10,40 @@ export function Logo({
   let settings = useThemeSettings();
   let { logoData, transparentLogoData, logoWidth } = settings;
 
-  if (!logoData) {
-    return null;
-  }
-
   return (
     <Link
       className="flex items-center justify-center w-full h-full lg:w-fit lg:h-fit z-30"
       to="/"
       prefetch="intent"
     >
-      <div className="relative" style={{ width: logoWidth }}>
-        <Image
-          data={logoData}
-          sizes="auto"
-          className={clsx(
-            "w-full h-full object-cover transition-opacity duration-300 ease-in group-hover/header:opacity-100",
-            isTransparent ? "opacity-0" : "opacity-100",
-          )}
-        />
-        <Image
-          data={transparentLogoData}
-          sizes="auto"
-          className={clsx(
-            "absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 ease-in group-hover/header:opacity-0",
-            isTransparent ? "opacity-100" : "opacity-0",
-          )}
-        />
+      <div
+        className="relative"
+        style={{ width: logoData ? logoWidth : "auto" }}
+      >
+        {logoData ? (
+          <>
+            <Image
+              data={logoData}
+              sizes="auto"
+              className={clsx(
+                "w-full h-full object-cover transition-opacity duration-300 ease-in group-hover/header:opacity-100",
+                isTransparent ? "opacity-0" : "opacity-100",
+              )}
+            />
+            <Image
+              data={transparentLogoData}
+              sizes="auto"
+              className={clsx(
+                "absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 ease-in group-hover/header:opacity-0",
+                isTransparent ? "opacity-100" : "opacity-0",
+              )}
+            />
+          </>
+        ) : (
+          <div className="text-lg sm:text-2xl font-medium line-clamp-1">
+            {shopName}
+          </div>
+        )}
       </div>
     </Link>
   );
