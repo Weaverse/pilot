849d3850fb16ff7b784738e404444f0f5732ac04
hta218 <huynhtuananh21895@gmail.com>
2024-07-20 22:39:09 +0700

    Updating product media

--- app/modules/product-form/product-media.tsx
+++ app/modules/product-form/product-media.tsx
@@ -1,8 +1,8 @@
 import { Image } from "@shopify/hydrogen";
-import clsx from "clsx";
-import { useKeenSlider } from "keen-slider/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { MediaFragment } from "storefrontapi.generated";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, type SwiperClass, SwiperSlide } from "swiper/react";
 
 interface ProductMediaProps {
   selectedVariant: any;
@@ -13,129 +13,56 @@ interface ProductMediaProps {
 }
 
 export function ProductMedia(props: ProductMediaProps) {
-  let {
-    selectedVariant,
-    media: _media,
-    showThumbnails,
-    numberOfThumbnails,
-    spacing,
-  } = props;
+  let { selectedVariant, media: _media, numberOfThumbnails, spacing } = props;
   let media = _media.filter((med) => med.__typename === "MediaImage");
-
-  let slideOptions = {
-    initial: 0,
-    loop: true,
-    slides: {
-      perView: 1,
-      spacing: 0,
-    },
-  };
-
-  let thumbnailOptions = {
-    initial: 0,
-    slides: {
-      perView: numberOfThumbnails,
-      spacing,
-    },
-  };
-
-  let [activeInd, setActiveInd] = useState(0);
-  let [sliderRef, instanceRef] = useKeenSlider({
-    ...slideOptions,
-    slideChanged: (slider) => {
-      let pos = slider.track.details.rel;
-      setActiveInd(pos);
-      let maxThumbnailIndex =
-        thumbnailInstance.current?.track.details.maxIdx || 0;
-      let thumbnailNext = Math.min(
-        Math.floor((pos + 1) / numberOfThumbnails),
-        maxThumbnailIndex,
-      );
-      thumbnailInstance.current?.moveToIdx(thumbnailNext);
-    },
-  });
-
-  function moveToIdx(idx: number) {
-    setActiveInd(idx);
-    if (instanceRef.current) {
-      instanceRef.current.moveToIdx(idx);
-    }
-  }
-
-  let [thumbnailRef, thumbnailInstance] = useKeenSlider(thumbnailOptions);
-
-  function handleClickThumbnail(idx: number) {
-    moveToIdx(idx);
-  }
-
-  useEffect(() => {
-    let selectedInd = media.findIndex((med) => {
-      if (med.__typename !== "MediaImage") return false;
-      return med.image?.url === selectedVariant?.image.url;
-    });
-    moveToIdx(selectedInd);
-    setTimeout(() => {
-      instanceRef.current?.update(slideOptions);
-      thumbnailInstance.current?.update(thumbnailOptions);
-    }, 100);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedVariant?.id]);
+  let [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
 
   return (
-    <div className="grid vt-product-image" style={{ gap: spacing }}>
-      <div ref={sliderRef} className="keen-slider">
+    <div className="flex flex-row-reverse gap-4" style={{ height: 800 }}>
+      <Swiper
+        modules={[Thumbs]}
+        spaceBetween={10}
+        thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+      >
         {media.map((med, i) => {
-          let image =
-            med.__typename === "MediaImage"
-              ? { ...med.image, altText: med.alt || "Product image" }
-              : null;
+          let image = { ...med.image, altText: med.alt || "Product image" };
           return (
-            image && (
-              <div className="keen-slider__slide" key={med.id}>
-                <Image
-                  data={image}
-                  loading={i === 0 ? "eager" : "lazy"}
-                  aspectRatio={"4/5"}
-                  className="object-cover w-full h-full aspect-square fadeIn"
-                  sizes={"auto"}
-                />
-              </div>
-            )
+            <SwiperSlide key={med.id}>
+              <Image
+                data={image}
+                loading={i === 0 ? "eager" : "lazy"}
+                aspectRatio={"3/4"}
+                className="object-cover w-full h-auto fadeIn"
+                sizes="auto"
+              />
+            </SwiperSlide>
           );
         })}
-      </div>
-      {showThumbnails && (
-        <div ref={thumbnailRef} className="keen-slider thumbnail">
-          {media.map((med, i) => {
-            let image =
-              med.__typename === "MediaImage"
-                ? { ...med.image, altText: med.alt || "Product image" }
-                : null;
-            return (
-              image && (
-                <div
-                  key={med.id}
-                  className={clsx(
-                    "keen-slider__slide border-2 cursor-pointer",
-                    i === activeInd ? "border-bar/70" : "",
-                  )}
-                  onClick={() => handleClickThumbnail(i)}
-                  role="listitem"
-                >
-                  <Image
-                    data={image}
-                    loading="lazy"
-                    width={200}
-                    sizes="100px 100w, 300px 300w, 500px 500w, 1000px 1000w"
-                    aspectRatio="1/1"
-                    className="object-cover w-full h-full aspect-square fadeIn"
-                  />
-                </div>
-              )
-            );
-          })}
-        </div>
-      )}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        direction="vertical"
+        spaceBetween={spacing}
+        freeMode
+        slidesPerView={5}
+        modules={[FreeMode, Thumbs]}
+        className="max-h-[450px]"
+      >
+        {media.map((med, i) => {
+          let image = { ...med.image, altText: med.alt || "Product image" };
+          return (
+            <SwiperSlide key={med.id}>
+              <Image
+                data={image}
+                loading={i === 0 ? "eager" : "lazy"}
+                aspectRatio={"3/4"}
+                className="object-cover h-auto fadeIn w-20"
+                sizes="auto"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 }
