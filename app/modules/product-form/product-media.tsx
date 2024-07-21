429805e396834d083b991b4b0dac9a2df06574e7
hta218 <huynhtuananh21895@gmail.com>
2024-07-21 11:14:33 +0700

    Update product media component with vertical swiper and responsive

--- app/modules/product-form/product-media.tsx
+++ app/modules/product-form/product-media.tsx
@@ -1,4 +1,5 @@
 import { Image } from "@shopify/hydrogen";
+import clsx from "clsx";
 import { useState } from "react";
 import type { MediaFragment } from "storefrontapi.generated";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
@@ -16,13 +17,58 @@ export function ProductMedia(props: ProductMediaProps) {
   let { selectedVariant, media: _media, numberOfThumbnails, spacing } = props;
   let media = _media.filter((med) => med.__typename === "MediaImage");
   let [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+  let [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="flex flex-row-reverse gap-4" style={{ height: 800 }}>
+    <div className="flex flex-col-reverse md:flex-row gap-4 overflow-hidden">
       <Swiper
-        modules={[Thumbs]}
+        onSwiper={setThumbsSwiper}
+        direction="vertical"
+        spaceBetween={spacing}
+        freeMode
+        slidesPerView={5}
+        threshold={2}
+        modules={[FreeMode, Thumbs]}
+        className="!w-20 shrink-0 max-h-[450px] overflow-visible hidden md:block"
+      >
+        {media.map((med, i) => {
+          let image = { ...med.image, altText: med.alt || "Product image" };
+          return (
+            <SwiperSlide
+              key={med.id}
+              className={clsx(
+                "!h-[100px] p-1 border transition-colors aspect-[3/4] cursor-pointer",
+                activeIndex === i ? "border-black" : "border-transparent",
+              )}
+            >
+              <Image
+                data={image}
+                loading={i === 0 ? "eager" : "lazy"}
+                width={100}
+                height={100}
+                aspectRatio={"3/4"}
+                className="object-cover fadeIn w-full h-full"
+                sizes="auto"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <Swiper
+        modules={[FreeMode, Thumbs]}
         spaceBetween={10}
-        thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+        thumbs={
+          thumbsSwiper
+            ? {
+                swiper: thumbsSwiper,
+                slideThumbActiveClass: "thumb-active",
+              }
+            : undefined
+        }
+        onSlideChange={(swiper) => {
+          setActiveIndex(swiper.activeIndex);
+        }}
+        className="max-w-full"
       >
         {media.map((med, i) => {
           let image = { ...med.image, altText: med.alt || "Product image" };
@@ -33,30 +79,6 @@ export function ProductMedia(props: ProductMediaProps) {
                 loading={i === 0 ? "eager" : "lazy"}
                 aspectRatio={"3/4"}
                 className="object-cover w-full h-auto fadeIn"
-                sizes="auto"
-              />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        direction="vertical"
-        spaceBetween={spacing}
-        freeMode
-        slidesPerView={5}
-        modules={[FreeMode, Thumbs]}
-        className="max-h-[450px]"
-      >
-        {media.map((med, i) => {
-          let image = { ...med.image, altText: med.alt || "Product image" };
-          return (
-            <SwiperSlide key={med.id}>
-              <Image
-                data={image}
-                loading={i === 0 ? "eager" : "lazy"}
-                aspectRatio={"3/4"}
-                className="object-cover h-auto fadeIn w-20"
                 sizes="auto"
               />
             </SwiperSlide>
