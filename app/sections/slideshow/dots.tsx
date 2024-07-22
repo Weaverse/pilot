920a6ff16f3654bf8a83dbff8523fc15dc614c71
hta218 <huynhtuananh21895@gmail.com>
2024-07-22 11:22:03 +0700

    Fix slideshow dots styles to prevent conflict w default swiper css

--- app/sections/slideshow/dots.tsx
+++ app/sections/slideshow/dots.tsx
@@ -9,15 +9,15 @@ export interface SlideshowDotsProps extends VariantProps<typeof variants> {
 let variants = cva(
   [
     "slideshow-dots",
-    "absolute z-1 flex justify-center items-center px-2.5 gap-4",
+    "absolute z-1 !w-auto flex justify-center items-center px-2.5 gap-4 w-",
   ],
   {
     variants: {
       dotsPosition: {
-        top: "left-0 right-0 top-10",
-        bottom: "left-0 right-0 bottom-10",
-        left: "top-0 bottom-0 flex-col left-5",
-        right: "top-0 bottom-0 flex-col right-5",
+        top: "!left-0 !right-0 !top-10 !bottom-auto",
+        bottom: "!left-0 !right-0 !bottom-10 !top-auto",
+        left: "!top-0 !bottom-0 flex-col !left-5 !right-auto",
+        right: "!top-0 !bottom-0 flex-col !right-5 !left-auto",
       },
       dotsColor: {
         light: "[&_.dot]:bg-white [&_.active]:!outline-white",
