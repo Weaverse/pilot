78db1175bb9f895c85449edd94c435599898dc43
hta218 <huynhtuananh21895@gmail.com>
2024-07-29 22:18:24 +0700

    Add `OurTeam` section

---
+++ app/sections/our-team.tsx
@@ -0,0 +1,197 @@
+import { Image } from "@shopify/hydrogen";
+import type {
+  ComponentLoaderArgs,
+  HydrogenComponentProps,
+  HydrogenComponentSchema,
+  WeaverseMetaObject,
+} from "@weaverse/hydrogen";
+import clsx from "clsx";
+import { forwardRef } from "react";
+
+type MetaObjectField = {
+  key: string;
+  type: string;
+  value: any;
+  reference?: any;
+};
+
+type MetaobjectData = {
+  handle: string;
+  id: string;
+  type: string;
+  fields: MetaObjectField[];
+};
+
+type MetaDemoProps = HydrogenComponentProps & {
+  title: string;
+  metaobject: WeaverseMetaObject;
+  itemsCount: number;
+  itemsPerRow: number;
+  gap: number;
+};
+
+let MetaDemo = forwardRef<HTMLDivElement, MetaDemoProps>((props, ref) => {
+  let {
+    loaderData,
+    metaobject,
+    itemsCount,
+    title,
+    itemsPerRow,
+    gap,
+    className,
+    ...rest
+  } = props;
+  if (!metaobject) {
+    return (
+      <section
+        className={clsx(
+          "w-full px-6 py-12 md:py-24 lg:py-32 mx-auto",
+          className,
+        )}
+        ref={ref}
+        {...rest}
+      >
+        skeleton goes here
+      </section>
+    );
+  }
+  let items = loaderData?.metaobjects.map(
+    (metaObject: MetaobjectData, ind: number) => {
+      let { fields } = metaObject;
+      let image = fields.find((field) => field.key === "avatar");
+      let imageData = image?.reference?.image;
+      let name = fields.find((field) => field.key === "name")?.value;
+      let title = fields.find((field) => field.key === "title")?.value;
+      return (
+        <div key={ind} className="flex flex-col gap-2 items-center">
+          <div className="rounded-md overflow-hidden w-44">
+            <Image
+              data={imageData}
+              sizes="auto"
+              className="h-auto"
+              aspectRatio="1/1"
+            />
+          </div>
+          <h3 className="font-semibold text-xl">{name}</h3>
+          <p>{title}</p>
+        </div>
+      );
+    },
+  );
+  return (
+    <section
+      className={clsx(
+        "max-w-7xl px-6 py-12 md:py-24 lg:py-32 w-fit mx-auto",
+        className,
+      )}
+      ref={ref}
+      {...rest}
+    >
+      <h2 className="text-center font-semibold mb-8">{title}</h2>
+      <div
+        className="grid w-fit mx-auto"
+        style={{
+          gridTemplateColumns: `repeat(${itemsPerRow}, minmax(0, 1fr))`,
+          gap: `${gap}px`,
+        }}
+      >
+        {items}
+      </div>
+    </section>
+  );
+});
+
+export let loader = async (args: ComponentLoaderArgs<MetaDemoProps>) => {
+  let { weaverse, data } = args;
+  let { storefront } = weaverse;
+  let { metaobject, itemsCount } = data;
+  if (metaobject) {
+    let { metaobjects } = await storefront.query(METAOBJECTS_QUERY, {
+      variables: {
+        type: metaobject.handle,
+        first: itemsCount,
+      },
+    });
+    return {
+      metaobjects: metaobjects.nodes,
+    };
+  }
+  return null;
+};
+
+let METAOBJECTS_QUERY = `#graphql
+  query MetaObjects ($type: String!, $first: Int) {
+    metaobjects(type: $type, first: $first) {
+      nodes {
+        fields {
+          key
+          type
+          value
+          reference {
+            ... on MediaImage {
+              alt
+              image {
+                altText
+                url
+                width
+                height
+              }
+            }
+          }
+        }
+        handle
+        id
+        type
+      }
+    }
+  }
+`;
+
+export let schema: HydrogenComponentSchema = {
+  type: "meta-demo",
+  title: "Metaobject Demo",
+  inspector: [
+    {
+      group: "Metaobject Demo",
+      inputs: [
+        {
+          label: "Select metaobject definition",
+          type: "metaobject",
+          helpText:
+            '<a href="https://weaverse.io/docs/marketplace/the-pilot-theme#metaobjects" target="_blank">How to display this demo section</a>',
+          name: "metaDemo",
+          shouldRevalidate: true,
+        },
+        {
+          label: "Title",
+          type: "text",
+          name: "title",
+          defaultValue: "Title",
+        },
+        {
+          label: "Items per row",
+          name: "itemsPerRow",
+          type: "range",
+          configs: {
+            min: 1,
+            max: 10,
+          },
+          defaultValue: 3,
+        },
+        {
+          label: "Gap between items",
+          name: "gap",
+          type: "range",
+          configs: {
+            min: 0,
+            step: 2,
+            max: 100,
+          },
+          defaultValue: 10,
+        },
+      ],
+    },
+  ],
+};
+
+export default MetaDemo;
