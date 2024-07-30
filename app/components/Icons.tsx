6aa0dc233be86ddeeca4da9ce17c0133fa4c1c61
hta218 <huynhtuananh21895@gmail.com>
2024-07-30 17:13:41 +0700

    Update Checkbox component to use new icon

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -419,3 +419,18 @@ export function IconGithubLogo(props: IconProps) {
     </svg>
   );
 }
+
+export function IconCheck(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z" />
+    </svg>
+  );
+}
