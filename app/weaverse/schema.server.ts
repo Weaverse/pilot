60affa8f5e77780502aafb55929e8b25d8355478
hta218 <huynhtuananh21895@gmail.com>
2024-08-12 18:10:41 +0700

    Simplify ids for sample swatches values

--- app/weaverse/schema.server.ts
+++ app/weaverse/schema.server.ts
@@ -2,40 +2,6 @@ import type { HydrogenThemeSchema } from "@weaverse/hydrogen";
 import type { SwatchesConfigs } from "~/types/weaverse-hydrogen";
 import pkg from "../../package.json";
 
-let uuid = () => {
-  let lut = [];
-  for (let i = 0; i < 256; i++) {
-    lut[i] = (i < 16 ? "0" : "") + i.toString(16);
-  }
-
-  let d0 = (Math.random() * 0xffffffff) | 0;
-  let d1 = (Math.random() * 0xffffffff) | 0;
-  let d2 = (Math.random() * 0xffffffff) | 0;
-  let d3 = (Math.random() * 0xffffffff) | 0;
-
-  return (
-    lut[d0 & 0xff] +
-    lut[(d0 >> 8) & 0xff] +
-    lut[(d0 >> 16) & 0xff] +
-    lut[(d0 >> 24) & 0xff] +
-    "-" +
-    lut[d1 & 0xff] +
-    lut[(d1 >> 8) & 0xff] +
-    "-" +
-    lut[((d1 >> 16) & 0x0f) | 0x40] +
-    lut[(d1 >> 24) & 0xff] +
-    "-" +
-    lut[(d2 & 0x3f) | 0x80] +
-    lut[(d2 >> 8) & 0xff] +
-    "-" +
-    lut[(d2 >> 16) & 0xff] +
-    lut[(d2 >> 24) & 0xff] +
-    lut[d3 & 0xff] +
-    lut[(d3 >> 8) & 0xff] +
-    lut[(d3 >> 16) & 0xff] +
-    lut[(d3 >> 24) & 0xff]
-  );
-};
 let swatchesConfigs: SwatchesConfigs = {
   options: [
     {
@@ -57,50 +23,50 @@ let swatchesConfigs: SwatchesConfigs = {
   ],
   swatches: {
     colors: [
-      { id: uuid(), name: "Red", value: "#ff0000" },
-      { id: uuid(), name: "Yellow", value: "#ffff00" },
-      { id: uuid(), name: "Black", value: "#000000" },
-      { id: uuid(), name: "Blue", value: "#0000FF" },
-      { id: uuid(), name: "Green", value: "#00ff00" },
-      { id: uuid(), name: "Purple", value: "#800080" },
-      { id: uuid(), name: "Silver", value: "#c0c0c0" },
-      { id: uuid(), name: "White", value: "#ffffff" },
-      { id: uuid(), name: "Brown", value: "#7B3F00" },
-      { id: uuid(), name: "Light-brown", value: "#feb035" },
-      { id: uuid(), name: "Dark-turquoise", value: "#23cddc" },
-      { id: uuid(), name: "Orange", value: "#fe9001" },
-      { id: uuid(), name: "Tan", value: "#eacea7" },
-      { id: uuid(), name: "Violet", value: "#EE82EE" },
-      { id: uuid(), name: "Pink", value: "#FFC0CB" },
-      { id: uuid(), name: "Grey", value: "#808080" },
+      { id: "1", name: "Red", value: "#ff0000" },
+      { id: "2", name: "Yellow", value: "#ffff00" },
+      { id: "3", name: "Black", value: "#000000" },
+      { id: "4", name: "Blue", value: "#0000FF" },
+      { id: "5", name: "Green", value: "#00ff00" },
+      { id: "6", name: "Purple", value: "#800080" },
+      { id: "7", name: "Silver", value: "#c0c0c0" },
+      { id: "8", name: "White", value: "#ffffff" },
+      { id: "9", name: "Brown", value: "#7B3F00" },
+      { id: "10", name: "Light-brown", value: "#feb035" },
+      { id: "11", name: "Dark-turquoise", value: "#23cddc" },
+      { id: "12", name: "Orange", value: "#fe9001" },
+      { id: "13", name: "Tan", value: "#eacea7" },
+      { id: "14", name: "Violet", value: "#EE82EE" },
+      { id: "15", name: "Pink", value: "#FFC0CB" },
+      { id: "16", name: "Grey", value: "#808080" },
     ],
     images: [
       {
-        id: uuid(),
+        id: "1",
         name: "Dark blue",
         value:
           "https://cdn.shopify.com/s/files/1/0838/0052/3057/files/swatch_pattern_2.png",
       },
       {
-        id: uuid(),
+        id: "2",
         name: "Light orange",
         value:
           "https://cdn.shopify.com/s/files/1/0838/0052/3057/files/swatch_pattern_3.png",
       },
       {
-        id: uuid(),
+        id: "3",
         name: "Dark red",
         value:
           "https://cdn.shopify.com/s/files/1/0838/0052/3057/files/swatch_pattern_4.png",
       },
       {
-        id: uuid(),
+        id: "4",
         name: "Light brown",
         value:
           "https://cdn.shopify.com/s/files/1/0838/0052/3057/files/swatch_pattern_5.png",
       },
       {
-        id: uuid(),
+        id: "5",
         name: "Dark brown",
         value:
           "https://cdn.shopify.com/s/files/1/0838/0052/3057/files/swatch_pattern_6.png",
