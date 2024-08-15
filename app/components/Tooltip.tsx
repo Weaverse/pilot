309b5f9ac3d23eb011009fcee2c8f00fd9e89a28
hta218 <huynhtuananh21895@gmail.com>
2024-08-15 11:26:19 +0700

    Update Tooltip component delay duration and side offset

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -15,7 +15,7 @@ import { forwardRef } from "react";
 import { cn } from "~/lib/cn";
 
 export let TooltipProvider = Provider;
-export let Tooltip = ({ delayDuration = 300, ...rest }: TooltipProps) => (
+export let Tooltip = ({ delayDuration = 100, ...rest }: TooltipProps) => (
   <Root delayDuration={delayDuration} {...rest} />
 );
 
@@ -25,13 +25,13 @@ export let TooltipTrigger = ({
 }: TooltipTriggerProps) => <Trigger asChild={asChild} {...rest} />;
 
 export let TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(
-  ({ children, className, sideOffset = 6, ...rest }, ref) => {
+  ({ children, className, sideOffset = 4, ...rest }, ref) => {
     return (
       <Content
         ref={ref}
         className={cn(
           "animate-slideDownAndFade",
-          "z-50 px-3 rounded py-1.5 shadow-sm text-background bg-body",
+          "z-50 px-4 rounded py-1.5 shadow-sm text-background bg-body opacity-0",
           className,
         )}
         align="center"
@@ -40,7 +40,7 @@ export let TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(
         collisionPadding={8}
         {...rest}
       >
-        <Arrow asChild offset={-2}>
+        <Arrow asChild>
           <span className="border-x-6 border-t-6 border-x-transparent border-t-body" />
         </Arrow>
         {children}
