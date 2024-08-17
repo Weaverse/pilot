5dae61d35d46c4cbb8969870f7ebfd4d67c3b2c5
hta218 <huynhtuananh21895@gmail.com>
2024-08-17 15:40:11 +0700

    Add IconTrash component and Skeleton component

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+
+/**
+ * A shared component and Suspense call that's used in `App.server.jsx` to let your app wait for code to load while declaring a loading state
+ */
+export function Skeleton({
+  as: Component = "div",
+  width,
+  height,
+  className,
+  ...props
+}: {
+  as?: React.ElementType;
+  width?: string;
+  height?: string;
+  className?: string;
+  [key: string]: any;
+}) {
+  return (
+    <Component
+      {...props}
+      width={width}
+      height={height}
+      className={clsx("rounded bg-body/10", className)}
+    />
+  );
+}
