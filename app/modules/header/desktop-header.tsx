739071e4f01da17e4e94de06f5b8e0fd6d4aa64c
hta218 <hta218.dev@gmail.com>
2024-11-19 09:52:19 +0700

    Allow setting content width for header

--- app/modules/header/desktop-header.tsx
+++ app/modules/header/desktop-header.tsx
@@ -6,7 +6,8 @@ import {
   useRouteLoaderData,
 } from "@remix-run/react";
 import { useThemeSettings } from "@weaverse/hydrogen";
-import { Suspense, useEffect, useState } from "react";
+import { cva } from "class-variance-authority";
+import { Suspense, useEffect } from "react";
 import useWindowScroll from "react-use/esm/useWindowScroll";
 import { IconMagnifyingGlass, IconUser } from "~/components/icons";
 import { Logo } from "~/components/logo";
@@ -18,6 +19,21 @@ import { Drawer, useDrawer } from "../drawer";
 import { CartCount } from "./cart-count";
 import { DesktopMenu } from "./menu/desktop-menu";
 
+let variants = cva("", {
+  variants: {
+    width: {
+      full: "w-full h-full",
+      stretch: "w-full h-full",
+      fixed: "w-full h-full max-w-page mx-auto",
+    },
+    padding: {
+      full: "",
+      stretch: "px-3 md:px-10 lg:px-16",
+      fixed: "px-3 md:px-4 lg:px-6 mx-auto",
+    },
+  },
+});
+
 export function DesktopHeader({
   menu,
   openCart,
@@ -27,7 +43,8 @@ export function DesktopHeader({
   menu?: EnhancedMenu;
   shopName: string;
 }) {
-  let { enableTransparentHeader, topbarHeight } = useThemeSettings();
+  let { enableTransparentHeader, topbarHeight, headerWidth } =
+    useThemeSettings();
   let isHome = useIsHomePath();
   let { y } = useWindowScroll();
   let { isOpen, openDrawer, closeDrawer } = useDrawer();
@@ -46,35 +63,42 @@ export function DesktopHeader({
       }
       className={cn(
         "transition-colors duration-300 ease-in-out",
-        "h-nav hidden lg:flex items-center z-10 justify-between leading-none gap-8",
-        "px-3 md:px-10 lg:px-16",
-        "text-[var(--color-header-text)] bg-[var(--color-header-bg)]",
-        "hover:text-[var(--color-header-text)] hover:bg-[var(--color-header-bg)]",
+        "hidden lg:block lg:w-full z-10",
+        "bg-[--color-header-bg] hover:bg-[--color-header-bg]",
         "border-b border-[rgb(230,230,230)]",
+        variants({ padding: headerWidth }),
         scrolled && "shadow-header",
         enableTransparent
           ? [
               "fixed top-[var(--topbar-height,var(--initial-topbar-height))] w-screen group/header",
               !scrolled &&
-                "text-[var(--color-transparent-header-text)] bg-transparent border-transparent",
+                "text-[--color-transparent-header-text] bg-transparent border-transparent",
             ]
-          : "sticky top-0"
+          : "sticky top-0",
       )}
     >
-      <Logo isTransparent={isTransparent} shopName={shopName} />
-      {menu && <DesktopMenu menu={menu} />}
-      <div className="flex items-center gap-1 z-1">
-        <SearchToggle
-          isOpen={isOpen}
-          openDrawer={openDrawer}
-          closeDrawer={closeDrawer}
-        />
-        <AccountLink className="relative flex items-center justify-center w-8 h-8 focus:ring-body/5" />
-        <CartCount
-          isHome={isHome}
-          openCart={openCart}
-          isTransparent={isTransparent}
-        />
+      <div
+        className={cn(
+          "h-nav flex items-center justify-between leading-none gap-8",
+          "text-[--color-header-text] hover:text-[--color-header-text]",
+          variants({ width: headerWidth }),
+        )}
+      >
+        <Logo isTransparent={isTransparent} shopName={shopName} />
+        {menu && <DesktopMenu menu={menu} />}
+        <div className="flex items-center gap-1 z-1">
+          <SearchToggle
+            isOpen={isOpen}
+            openDrawer={openDrawer}
+            closeDrawer={closeDrawer}
+          />
+          <AccountLink className="relative flex items-center justify-center w-8 h-8 focus:ring-body/5" />
+          <CartCount
+            isHome={isHome}
+            openCart={openCart}
+            isTransparent={isTransparent}
+          />
+        </div>
       </div>
     </header>
   );
