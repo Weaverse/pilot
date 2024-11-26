446d371f086eb26a62d2f5d1da42485bff6724cb
hta218 <hta218.dev@gmail.com>
2024-11-26 18:38:48 +0700

    Update menu functionality

--- app/modules/header/menu/desktop-menu.tsx
+++ app/modules/header/menu/desktop-menu.tsx
@@ -1,186 +1,184 @@
+import { CaretDown } from "@phosphor-icons/react";
+import * as Menubar from "@radix-ui/react-menubar";
 import { Image } from "@shopify/hydrogen";
-import React from "react";
-import { IconCaretDown } from "~/components/icons";
-import { Link } from "~/components/link";
+import { useThemeSettings } from "@weaverse/hydrogen";
+import clsx from "clsx";
+import { useState } from "react";
+import Link from "~/components/link";
 import { cn } from "~/lib/cn";
 import { getMaxDepth } from "~/lib/menu";
 import type { SingleMenuItem } from "~/lib/type";
 import type { EnhancedMenu } from "~/lib/utils";
 
 export function DesktopMenu({ menu }: { menu: EnhancedMenu }) {
-  let items = menu.items as unknown as SingleMenuItem[];
-  if (items.length) {
+  let { openMenuBy } = useThemeSettings();
+  let [value, setValue] = useState<string | null>(null);
+  if (menu?.items?.length) {
+    let items = menu.items as unknown as SingleMenuItem[];
     return (
-      <nav className="flex items-stretch h-full">
-        {items.map((item) => {
-          let { id, items = [] } = item;
-          let level = getMaxDepth(item);
-          let isAllImageItems =
-            items.length &&
-            items.every(
-              (cItem) => cItem?.resource?.image && cItem.items.length === 0,
+      <Menubar.Root
+        asChild
+        value={value}
+        onValueChange={setValue}
+        onMouseLeave={() => setValue(null)}
+      >
+        <nav className="flex grow justify-center h-full">
+          {items.map((menuItem) => {
+            let { id, items = [], title, to } = menuItem;
+            let level = getMaxDepth(menuItem);
+            let hasSubmenu = level > 1;
+            let isDropdown =
+              level === 2 && items.every(({ resource }) => !resource?.image);
+            return (
+              <Menubar.Menu key={id} value={id}>
+                <Menubar.Trigger
+                  asChild={!hasSubmenu}
+                  className={clsx([
+                    "cursor-pointer px-3 py-2 h-full flex items-center gap-1.5",
+                    '[&>svg]:data-[state="open"]:rotate-180',
+                    "focus:outline-none uppercase",
+                  ])}
+                  onMouseEnter={() => {
+                    if (openMenuBy === "hover" && value !== id) {
+                      setValue(id);
+                    }
+                  }}
+                >
+                  {hasSubmenu ? (
+                    <>
+                      <span>{title}</span>
+                      <CaretDown className="w-3.5 h-3.5 transition-transform" />
+                    </>
+                  ) : (
+                    <Link to={to} className="transition-none">
+                      {title}
+                    </Link>
+                  )}
+                </Menubar.Trigger>
+                {level > 1 && (
+                  <Menubar.Content
+                    className={cn([
+                      "px-3 md:px-4 lg:px-6",
+                      "bg-[--color-header-bg] shadow-lg",
+                      isDropdown ? "py-6" : "w-screen py-8",
+                    ])}
+                  >
+                    {isDropdown ? (
+                      <DropdownSubMenu items={items} />
+                    ) : (
+                      <MegaMenu items={items} />
+                    )}
+                  </Menubar.Content>
+                )}
+              </Menubar.Menu>
             );
-          let MenuComponent: React.FC<SingleMenuItem> = isAllImageItems
-            ? ImagesMenu
-            : level > 2
-              ? MegaMenu
-              : level === 2
-                ? DropdownMenu
-                : FirstLevelMenu;
-
-          return <MenuComponent key={id} {...item} />;
-        })}
-      </nav>
+          })}
+        </nav>
+      </Menubar.Root>
     );
   }
   return null;
 }
 
-function MegaMenu({ title, items, to }: SingleMenuItem) {
-  let renderList = (item: SingleMenuItem, idx: number) => (
-    <div
-      key={item.id}
-      className="flex-1 fly-in max-w-60 space-y-4"
-      style={{ "--item-index": idx } as React.CSSProperties}
+function DropdownSubMenu({ items }: { items: SingleMenuItem[] }) {
+  return (
+    <ul
+      className="space-y-1.5 animate-fade-in"
+      style={{ "--fade-in-duration": "150ms" } as React.CSSProperties}
     >
-      <Link
-        to={item.to}
-        prefetch="intent"
-        className="uppercase transition-none"
-      >
-        <span className="reveal-underline">{item.title}</span>
-      </Link>
-      <ul className="space-y-1.5">
-        {item.items.map((subItem) => (
-          <li key={subItem.id} className="leading-6">
-            <Link
-              to={subItem.to}
-              prefetch="intent"
-              className="relative transition-none"
-            >
-              <span className="reveal-underline">{subItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  let renderImageItem = (item: SingleMenuItem, idx: number) => (
-    <div
-      className="flex-1 w-full aspect-[4/5] relative group/item overflow-hidden fly-in max-w-60"
-      key={idx}
-      style={{ "--item-index": idx } as React.CSSProperties}
-    >
-      <Link to={item.to} prefetch="intent" className="transition-none">
-        <Image
-          sizes="auto"
-          data={item.resource?.image}
-          className="w-full h-full object-cover group-hover/item:scale-110 transition-transform duration-300"
-        />
-      </Link>
-      <div className="absolute w-full inset-0 text-center text-white font-medium pointer-events-none p-2 bg-black/15 group-hover/item:bg-black/30 flex items-center justify-center transition-all duration-300">
-        {item.title}
-      </div>
-    </div>
-  );
-  return (
-    <FirstLevelMenu title={title} to={to}>
-      <div className="w-screen top-full left-0 absolute shadow-lg overflow-hidden bg-white z-10 dropdown-transition">
-        <div className="container mx-auto py-8">
-          <div className="flex gap-4 w-full">
-            {items.map((item, id) =>
-              item.resource && item.items.length === 0
-                ? renderImageItem(item, id)
-                : renderList(item, id),
-            )}
-          </div>
-          <div className="flex gap-6" />
-        </div>
-      </div>
-    </FirstLevelMenu>
-  );
-}
-
-function DropdownMenu(props: SingleMenuItem) {
-  let { title, items, to } = props;
-  return (
-    <FirstLevelMenu title={title} to={to} className="relative">
-      <div className="top-full -left-3 group-hover:h-auto absolute shadow-lg overflow-hidden bg-white z-10 dropdown-transition">
-        <div className="p-6 min-w-48">
-          <ul className="space-y-1.5">
-            {items.map((childItem) => (
-              <li key={childItem.id} className="leading-6">
-                <Link
-                  to={childItem.to}
-                  prefetch="intent"
-                  className="transition-none"
-                >
-                  <span className="reveal-underline">{childItem.title}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </FirstLevelMenu>
-  );
-}
-
-function ImagesMenu({ title, items, to }: SingleMenuItem) {
-  return (
-    <FirstLevelMenu title={title} to={to}>
-      <div className="w-screen top-full left-0 absolute shadow-lg overflow-hidden bg-white z-10 dropdown-transition">
-        <div className="py-8">
-          <div className="flex gap-6 w-fit container mx-auto">
-            {items.map((item, id) => (
-              <Link
-                to={item.to}
-                prefetch="intent"
-                key={item.id}
-                className="flex-1 fly-in transition-none"
-                style={{ "--item-index": id } as React.CSSProperties}
-              >
-                <div className="aspect-square relative group/item overflow-hidden">
-                  <Image
-                    data={item?.resource?.image}
-                    className="w-full h-full object-cover group-hover/item:scale-105 transition-transform duration-400"
-                    sizes="auto"
-                  />
-                  <h6 className="absolute w-full inset-0 text-center text-background p-2 bg-gray-100/15 group-hover/item:bg-gray-100/40 flex items-center justify-center transition-all duration-300">
-                    {item.title}
-                  </h6>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
-    </FirstLevelMenu>
-  );
-}
-
-function FirstLevelMenu(props: {
-  className?: string;
-  title: string;
-  to: string;
-  children?: React.ReactNode;
-}) {
-  let { children, className, title, to } = props;
-  let hasChild = React.Children.count(children) > 0;
-  return (
-    <div className={cn("group", className)}>
-      <div className="h-full flex items-center px-3 cursor-pointer relative z-30">
+      {items.map(({ id, to, title }) => (
         <Link
+          key={id}
           to={to}
-          className="py-2 flex items-center gap-1.5 transition-none"
+          prefetch="intent"
+          className="transition-none block"
         >
-          <span className="uppercase reveal-underline !pb-[5px]">{title}</span>
-          {hasChild && (
-            <IconCaretDown className="w-3 h-3 mb-[3px] group-hover:rotate-180 transition-transform duration-400" />
-          )}
+          <span className="reveal-underline">{title}</span>
         </Link>
-      </div>
+      ))}
+    </ul>
+  );
+}
+
+function MegaMenu({ items }: { items: SingleMenuItem[] }) {
+  return (
+    <div className="max-w-page mx-auto flex gap-4">
+      {items.map(({ id, title, to, items: children, resource }, idx) =>
+        resource?.image && children.length === 0 ? (
+          <SlideIn
+            key={id}
+            className="grow max-w-72 aspect-square relative group/item overflow-hidden"
+            style={{ "--idx": idx } as React.CSSProperties}
+          >
+            <Image
+              sizes="auto"
+              data={resource.image}
+              className="w-full h-full object-cover group-hover/item:scale-[1.03] transition-transform duration-300"
+            />
+            <Link
+              to={to}
+              prefetch="intent"
+              className={clsx([
+                "absolute inset-0 p-2 flex items-center justify-center text-center",
+                "bg-black/20 group-hover/item:bg-black/40",
+                "h6 text-body-inverse font-medium transition-all duration-300",
+              ])}
+            >
+              {title}
+            </Link>
+          </SlideIn>
+        ) : (
+          <SlideIn
+            key={id}
+            className="grow max-w-72 space-y-4"
+            style={{ "--idx": idx } as React.CSSProperties}
+          >
+            <Link
+              to={to}
+              prefetch="intent"
+              className="uppercase inline transition-none"
+            >
+              <span className="reveal-underline">{title}</span>
+            </Link>
+            <div className="flex flex-col gap-1.5">
+              {children.map((cItem) => (
+                <Link
+                  key={cItem.id}
+                  to={cItem.to}
+                  prefetch="intent"
+                  className="relative inline transition-none"
+                >
+                  <span className="reveal-underline">{cItem.title}</span>
+                </Link>
+              ))}
+            </div>
+          </SlideIn>
+        ),
+      )}
+    </div>
+  );
+}
+
+function SlideIn(props: {
+  className?: string;
+  children: React.ReactNode;
+  style: React.CSSProperties;
+}) {
+  let { className, children, style } = props;
+  return (
+    <div
+      className={cn(
+        "opacity-0 animate-slide-left-and-fade [animation-delay:calc(var(--idx)*0.1s+0.1s)]",
+        className,
+      )}
+      style={
+        {
+          "--left-distance": "60px",
+          "--slide-left-and-fade-duration": "200ms",
+          ...style,
+        } as React.CSSProperties
+      }
+    >
       {children}
     </div>
   );
