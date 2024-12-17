3b64f55cbcbaa101990e95216bfc65660f0bd90e
hta218 <hta218.dev@gmail.com>
2024-12-17 15:32:26 +0700

    Refine account routes

--- app/routes/($locale).account.$.tsx
+++ app/routes/($locale).account.$.tsx
@@ -3,7 +3,6 @@ import { type LoaderFunctionArgs, redirect } from "@shopify/remix-oxygen";
 // fallback wild card for all unauthenticated routes in account section
 export async function loader({ context, params }: LoaderFunctionArgs) {
   await context.customerAccount.handleAuthStatus();
-
-  const locale = params.locale;
+  let locale = params.locale;
   return redirect(locale ? `/${locale}/account` : "/account");
 }
