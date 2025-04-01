100f2282a31be52d52bfa7a0b01543c0b4c18618
Paul <phanminh65@gmail.com>
2025-04-01 16:10:02 +0700

    refactor: migrate Layout component to root.tsx since styling is lost for error boundary
    - Moved Layout component implementation from layout.tsx to root.tsx for better organization.
    - Updated routing to directly use flatRoutes, removing the previous layout dependency.
    - Enhanced the structure of the Layout component with improved data handling and styling.

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,15 @@
 import {
+  Links,
+  Meta,
   Outlet,
+  Scripts,
+  ScrollRestoration,
   isRouteErrorResponse,
   useRouteError,
+  useRouteLoaderData,
 } from "@remix-run/react";
 import type { SeoConfig } from "@shopify/hydrogen";
-import { getSeoMeta } from "@shopify/hydrogen";
+import { Analytics, getSeoMeta, useNonce } from "@shopify/hydrogen";
 import type {
   LinksFunction,
   LoaderFunctionArgs,
@@ -13,6 +18,21 @@ import type {
 import { GenericError } from "./components/root/generic-error";
 import { NotFound } from "./components/root/not-found";
 import { loadCriticalData, loadDeferredData } from "./utils/root.server";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { useThemeSettings, withWeaverse } from "@weaverse/hydrogen";
+import type { CSSProperties } from "react";
+import { Footer } from "./components/layout/footer";
+import { ScrollingAnnouncement } from "./components/layout/scrolling-announcement";
+import { CustomAnalytics } from "./components/root/custom-analytics";
+import { GlobalLoading } from "./components/root/global-loading";
+import { DEFAULT_LOCALE } from "./utils/const";
+import { GlobalStyle } from "./weaverse/style";
+import { Header } from "./components/layout/header";
+import poppins400 from "@fontsource/poppins/400.css?url";
+import poppins500 from "@fontsource/poppins/500.css?url";
+import poppins600 from "@fontsource/poppins/600.css?url";
+import poppins700 from "@fontsource/poppins/700.css?url";
+import styles from "./styles/app.css?url";
 
 export type RootLoader = typeof loader;
 
@@ -47,7 +67,7 @@ export let meta = ({ data }: MetaArgs<typeof loader>) => {
   return getSeoMeta(data?.seo as SeoConfig);
 };
 
-export default function App() {
+function App() {
   return <Outlet />;
 }
 
@@ -77,3 +97,73 @@ export function ErrorBoundary({ error }: { error: Error }) {
     <GenericError error={error instanceof Error ? error : undefined} />
   );
 }
+
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  let nonce = useNonce();
+  let data = useRouteLoaderData<RootLoader>("root");
+  let locale = data?.selectedLocale ?? DEFAULT_LOCALE;
+  let { topbarHeight, topbarText } = useThemeSettings();
+
+  return (
+    <html lang={locale.language}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href={poppins400} />
+        <link rel="stylesheet" href={poppins500} />
+        <link rel="stylesheet" href={poppins600} />
+        <link rel="stylesheet" href={poppins700} />
+        <link rel="stylesheet" href={styles} />
+        <Meta />
+        <Links />
+        <GlobalStyle />
+      </head>
+      <body
+        style={
+          {
+            opacity: 0,
+            "--initial-topbar-height": `${topbarText ? topbarHeight : 0}px`,
+          } as CSSProperties
+        }
+        className="transition-opacity !opacity-100 duration-300 antialiased bg-background text-body"
+      >
+        {data ? (
+          <Analytics.Provider
+            cart={data.cart}
+            shop={data.shop}
+            consent={data.consent}
+          >
+            <TooltipProvider disableHoverableContent>
+              <div
+                className="flex flex-col min-h-screen"
+                key={`${locale.language}-${locale.country}`}
+              >
+                <div className="">
+                  <a href="#mainContent" className="sr-only">
+                    Skip to content
+                  </a>
+                </div>
+                <ScrollingAnnouncement />
+                <Header />
+                <main id="mainContent" className="flex-grow">
+                  {children}
+                </main>
+                <Footer />
+              </div>
+            </TooltipProvider>
+            <CustomAnalytics />
+          </Analytics.Provider>
+        ) : (
+          children
+        )}
+        <GlobalLoading />
+        <ScrollRestoration nonce={nonce} />
+        <Scripts nonce={nonce} />
+      </body>
+    </html>
+  );
+}
+
+export default withWeaverse(App);
--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,3 @@
 import { flatRoutes } from "@remix-run/fs-routes";
-import { type RouteConfig, layout } from "@remix-run/route-config";
-import { hydrogenRoutes } from "@shopify/hydrogen";
 
-export default hydrogenRoutes([
-  // Your entire app reading from routes folder using Layout from layout.tsx
-  layout("./layout.tsx", await flatRoutes()),
-]) satisfies RouteConfig;
+export default flatRoutes();
