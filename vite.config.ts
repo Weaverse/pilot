2e696e0cf6a2eacfdfd8ae3b909055d7a2f7daeb
Paul <phanminh65@gmail.com>
2025-04-21 20:25:22 +0700

    Refactor Vite config for improved readability and consistency; standardize import formatting and update dependency inclusion list.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { hydrogen } from "@shopify/hydrogen/vite";
-import { oxygen } from "@shopify/mini-oxygen/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-declare module "@remix-run/server-runtime" {
+import {vitePlugin as remix} from '@remix-run/dev';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
+import {defineConfig} from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+declare module '@remix-run/server-runtime' {
   interface Future {
     v3_singleFetch: true;
   }
@@ -33,30 +33,31 @@ export default defineConfig({
   server: {
     warmup: {
       clientFiles: [
-        "./app/routes/**/*",
-        "./app/sections/**/*",
-        "./app/components/**/*",
+        './app/routes/**/*',
+        './app/sections/**/*',
+        './app/components/**/*',
       ],
     },
   },
   ssr: {
     optimizeDeps: {
       include: [
-        "jsonp",
-        "classnames",
-        "typographic-trademark",
-        "typographic-single-spaces",
-        "typographic-registered-trademark",
-        "typographic-math-symbols",
-        "typographic-en-dashes",
-        "typographic-em-dashes",
-        "typographic-ellipses",
-        "typographic-currency",
-        "typographic-copyright",
-        "typographic-apostrophes-for-possessive-plurals",
-        "typographic-quotes",
-        "typographic-apostrophes",
-        "textr",
+        '@radix-ui/react-primitive',
+        'jsonp',
+        'classnames',
+        'typographic-trademark',
+        'typographic-single-spaces',
+        'typographic-registered-trademark',
+        'typographic-math-symbols',
+        'typographic-en-dashes',
+        'typographic-em-dashes',
+        'typographic-ellipses',
+        'typographic-currency',
+        'typographic-copyright',
+        'typographic-apostrophes-for-possessive-plurals',
+        'typographic-quotes',
+        'typographic-apostrophes',
+        'textr',
       ],
     },
   },
