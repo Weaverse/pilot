4b1481276e16ea60d0409c31136b586a9692571a
hta218 <hta218.dev@gmail.com>
2025-05-24 17:36:21 +0700

    Using tailwindcss as a vite plugin
    - Added a clean script to package.json for easier cleanup of build artifacts.
    - Updated @tailwindcss/vite to version 4.1.7 in package.json.
    - Upgraded tailwind-merge from version 2.6.0 to 3.3.0 in package.json.
    - Removed @tailwindcss/postcss from postcss.config.mjs as it's no longer needed.
    - Integrated tailwindcss plugin into vite.config.ts for improved Tailwind CSS support.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,5 @@
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
     "postcss-preset-env": {
       features: { "nesting-rules": false },
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import {vitePlugin as remix} from '@remix-run/dev';
-import {hydrogen} from '@shopify/hydrogen/vite';
-import {oxygen} from '@shopify/mini-oxygen/vite';
-import {defineConfig} from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { vitePlugin as remix } from '@remix-run/dev'
+import { hydrogen } from '@shopify/hydrogen/vite'
+import { oxygen } from '@shopify/mini-oxygen/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 declare module '@remix-run/server-runtime' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 export default defineConfig({
@@ -23,6 +24,7 @@ export default defineConfig({
         v3_routeConfig: true,
       },
     }),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   build: {
@@ -61,4 +63,4 @@ export default defineConfig({
       ],
     },
   },
-});
+})
