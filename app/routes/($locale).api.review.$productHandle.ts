3d62f34dfb51748d7546c74fc4fdd48a558ae9a5
Paul <phanminh65@gmail.com>
2025-06-11 16:43:35 +0700

    Refactor tsconfig.json by removing typeRoots, update import order in entry.server.tsx, and simplify loader function in ($locale).api.review.$productHandle.ts for improved clarity.

--- app/routes/($locale).api.review.$productHandle.ts
+++ app/routes/($locale).api.review.$productHandle.ts
@@ -1,10 +1,7 @@
 import invariant from "tiny-invariant";
 import { getJudgeMeProductReviews } from "~/utils/judgeme";
 
-export async function loader({
-  params: { productHandle },
-  context,
-}) {
+export async function loader({ params: { productHandle }, context }) {
   invariant(productHandle, "Missing product handle");
   return await getJudgeMeProductReviews({ context, handle: productHandle });
 }
