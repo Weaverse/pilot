2c748703b141ac0bb34d6a44e342127232edb8d3
hta218 <hta218.dev@gmail.com>
2025-07-22 09:52:52 +0700

    Show only sale badge in cart best seller box

--- app/components/cart/cart-best-sellers.tsx
+++ app/components/cart/cart-best-sellers.tsx
@@ -60,6 +60,7 @@ export function CartBestSellers({
       <div
         className={clsx([
           "grid grid-cols-2 gap-x-6 gap-y-8",
+          "[&_.bundle-badge,&_.new-badge,&_.best-seller-badge]:hidden",
           layout === "page" ? "sm:grid-cols-4 md:grid-cols-4" : "",
         ])}
       >
