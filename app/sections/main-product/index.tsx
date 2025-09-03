bb56ccf7fc372b7b369196cf1f402edb09faddf8
hta218 <hta218.dev@gmail.com>
2025-09-03 15:01:24 +0700

    Refactor MainProduct component to simplify structure

--- app/sections/main-product/index.tsx
+++ app/sections/main-product/index.tsx
@@ -1,69 +1,26 @@
-import {
-  getAdjacentAndFirstAvailableVariants,
-  getProductOptions,
-  ShopPayButton,
-  useOptimisticVariant,
-} from "@shopify/hydrogen";
-import type { ProductVariantComponent } from "@shopify/hydrogen/storefront-api-types";
 import { createSchema } from "@weaverse/hydrogen";
 import clsx from "clsx";
-import { forwardRef, useState } from "react";
+import { forwardRef } from "react";
 import { useLoaderData } from "react-router";
-import { Link } from "~/components/link";
-import { AddToCartButton } from "~/components/product/add-to-cart-button";
-import { ProductBadges } from "~/components/product/badges";
-import { BundledVariants } from "~/components/product/bundled-variants";
 import {
   ProductMedia,
   type ProductMediaProps,
 } from "~/components/product/product-media";
-import { Quantity } from "~/components/product/quantity";
-import { VariantPrices } from "~/components/product/variant-prices";
 import { layoutInputs, Section, type SectionProps } from "~/components/section";
 import type { loader as productRouteLoader } from "~/routes/($locale).products.$productHandle";
 import { isCombinedListing } from "~/utils/combined-listings";
-import { ProductDetails } from "./product-details";
-import { ProductVariants } from "./variants";
+import { ProductQuantityProvider } from "./product-quantity-context";
 
 interface ProductInformationData
-  extends Omit<ProductMediaProps, "selectedVariant" | "media"> {
-  addToCartText: string;
-  addBundleToCartText: string;
-  soldOutText: string;
-  showVendor: boolean;
-  showSalePrice: boolean;
-  showShortDescription: boolean;
-  showShippingPolicy: boolean;
-  showRefundPolicy: boolean;
-}
+  extends Omit<ProductMediaProps, "selectedVariant" | "media"> {}
 
 const ProductInformation = forwardRef<
   HTMLDivElement,
   ProductInformationData & SectionProps
 >((props, ref) => {
-  const { product, storeDomain } = useLoaderData<typeof productRouteLoader>();
-
-  // Optimistically selects a variant with given available variant information
-  const selectedVariant = useOptimisticVariant(
-    product?.selectedOrFirstAvailableVariant,
-    getAdjacentAndFirstAvailableVariants(product),
-  );
-
-  // Get the product options array
-  const productOptions = getProductOptions({
-    ...product,
-    selectedOrFirstAvailableVariant: selectedVariant,
-  });
+  const { product } = useLoaderData<typeof productRouteLoader>();
 
   const {
-    addToCartText,
-    addBundleToCartText,
-    soldOutText,
-    showVendor,
-    showSalePrice,
-    showShortDescription,
-    showShippingPolicy,
-    showRefundPolicy,
     mediaLayout,
     gridSize,
     imageAspectRatio,
@@ -74,20 +31,11 @@ const ProductInformation = forwardRef<
     zoomButtonVisibility,
     ...rest
   } = props;
-  const [quantity, setQuantity] = useState<number>(1);
 
-  const isBundle = Boolean(product?.isBundle?.requiresComponents);
-  const bundledVariants = isBundle ? product?.isBundle?.components.nodes : null;
   const combinedListing = isCombinedListing(product);
 
   if (product) {
-    const { title, handle, vendor, summary } = product;
-    let atcButtonText = "Add to cart";
-    if (selectedVariant.availableForSale) {
-      atcButtonText = isBundle ? addBundleToCartText : addToCartText;
-    } else {
-      atcButtonText = soldOutText;
-    }
+    const { handle } = product;
 
     return (
       <Section ref={ref} {...rest} overflow="unset">
@@ -118,7 +66,7 @@ const ProductInformation = forwardRef<
                   ]
                 : product?.media?.nodes || []
             }
-            selectedVariant={selectedVariant}
+            selectedVariant={product?.selectedOrFirstAvailableVariant}
             showThumbnails={showThumbnails}
             enableZoom={enableZoom}
             zoomTrigger={zoomTrigger}
@@ -126,107 +74,12 @@ const ProductInformation = forwardRef<
           />
           <div>
             <div
-              className="sticky flex flex-col justify-start space-y-5"
+              className="sticky flex flex-col justify-start gap-5"
               style={{ top: "calc(var(--height-nav) + 20px)" }}
             >
-              <div className="flex items-center gap-1.5">
-                <Link
-                  to="/"
-                  className="text-body-subtle underline-offset-4 hover:underline"
-                >
-                  Home
-                </Link>
-                <span className="inline-block h-4 border-body-subtle border-r" />
-                <span>{product.title}</span>
-              </div>
-              <ProductBadges
-                product={product}
-                selectedVariant={selectedVariant}
-              />
-              <div className="flex flex-col gap-2">
-                {showVendor && vendor && (
-                  <span className="text-body-subtle">{vendor}</span>
-                )}
-                <h1 className="h3 tracking-tight!">{title}</h1>
-              </div>
-              {combinedListing ? (
-                <div className="flex gap-2 text-2xl/none">
-                  <span className="flex gap-1">
-                    From
-                    <VariantPrices
-                      variant={{ price: product.priceRange.minVariantPrice }}
-                      showCompareAtPrice={false}
-                    />
-                  </span>
-                  <span className="flex gap-1">
-                    To
-                    <VariantPrices
-                      variant={{ price: product.priceRange.maxVariantPrice }}
-                      showCompareAtPrice={false}
-                    />
-                  </span>
-                </div>
-              ) : (
-                <VariantPrices
-                  variant={selectedVariant}
-                  showCompareAtPrice={showSalePrice}
-                  className="text-2xl/none"
-                />
-              )}
-              {children}
-              {showShortDescription && (
-                <p className="leading-relaxed">{summary}</p>
-              )}
-              {isBundle && (
-                <div className="space-y-3">
-                  <h4 className="text-2xl">Bundled Products</h4>
-                  <BundledVariants
-                    variants={bundledVariants as ProductVariantComponent[]}
-                  />
-                </div>
-              )}
-              <ProductVariants
-                productOptions={productOptions}
-                selectedVariant={selectedVariant}
-                combinedListing={combinedListing}
-              />
-              {!combinedListing && (
-                <>
-                  <Quantity value={quantity} onChange={setQuantity} />
-                  <div className="space-y-2">
-                    <AddToCartButton
-                      disabled={!selectedVariant?.availableForSale}
-                      lines={[
-                        {
-                          merchandiseId: selectedVariant?.id,
-                          quantity,
-                          selectedVariant,
-                        },
-                      ]}
-                      data-test="add-to-cart"
-                      className="w-full uppercase"
-                    >
-                      {atcButtonText}
-                    </AddToCartButton>
-                    {selectedVariant?.availableForSale && (
-                      <ShopPayButton
-                        width="100%"
-                        variantIdsAndQuantities={[
-                          {
-                            id: selectedVariant?.id,
-                            quantity,
-                          },
-                        ]}
-                        storeDomain={storeDomain}
-                      />
-                    )}
-                  </div>
-                </>
-              )}
-              <ProductDetails
-                showShippingPolicy={showShippingPolicy}
-                showRefundPolicy={showRefundPolicy}
-              />
+              <ProductQuantityProvider>
+                {children}
+              </ProductQuantityProvider>
             </div>
           </div>
         </div>
@@ -245,7 +98,20 @@ export default ProductInformation;
 export const schema = createSchema({
   type: "main-product",
   title: "Main product",
-  childTypes: ["judgeme-reviews-badge"],
+  childTypes: [
+    "mp--breadcrumb",
+    "mp--badges",
+    "mp--vendor",
+    "mp--title",
+    "mp--prices",
+    "judgeme-reviews-badge",
+    "mp--summary",
+    "mp--bundled-variants",
+    "mp--variant-selector",
+    "mp--quantity-selector",
+    "mp--atc-buttons",
+    "mp--details-accordion",
+  ],
   limit: 1,
   enabledOn: {
     pages: ["PRODUCT"],
@@ -349,65 +215,64 @@ export const schema = createSchema({
         },
       ],
     },
-    {
-      group: "Product information",
-      inputs: [
-        {
-          type: "text",
-          label: "Add to cart text",
-          name: "addToCartText",
-          defaultValue: "Add to cart",
-          placeholder: "Add to cart",
-        },
-        {
-          type: "text",
-          label: "Bundle add to cart text",
-          name: "addBundleToCartText",
-          defaultValue: "Add bundle to cart",
-          placeholder: "Add bundle to cart",
-        },
-        {
-          type: "text",
-          label: "Sold out text",
-          name: "soldOutText",
-          defaultValue: "Sold out",
-          placeholder: "Sold out",
-        },
-        {
-          type: "switch",
-          label: "Show vendor",
-          name: "showVendor",
-          defaultValue: false,
-        },
-        {
-          type: "switch",
-          label: "Show sale price",
-          name: "showSalePrice",
-          defaultValue: true,
-        },
-        {
-          type: "switch",
-          label: "Show short description",
-          name: "showShortDescription",
-          defaultValue: true,
-        },
-        {
-          type: "switch",
-          label: "Show shipping policy",
-          name: "showShippingPolicy",
-          defaultValue: true,
-        },
-        {
-          type: "switch",
-          label: "Show refund policy",
-          name: "showRefundPolicy",
-          defaultValue: true,
-        },
-      ],
-    },
   ],
   presets: {
     mediaLayout: "grid",
     gridSize: "2x2",
+    children: [
+      {
+        type: "mp--breadcrumb",
+        homeText: "Home",
+      },
+      {
+        type: "mp--badges",
+      },
+      {
+        type: "mp--vendor",
+        showVendor: false,
+      },
+      {
+        type: "mp--title",
+        headingLevel: "h1",
+        className: "h3 tracking-tight!",
+      },
+      {
+        type: "mp--prices",
+        showCompareAtPrice: true,
+        className: "text-2xl/none",
+      },
+      {
+        type: "mp--reviews-badge",
+      },
+      {
+        type: "mp--summary",
+        showSummary: true,
+        className: "leading-relaxed",
+      },
+      {
+        type: "mp--bundled-variants",
+        headingText: "Bundled Products",
+        headingClassName: "text-2xl",
+      },
+      {
+        type: "mp--variant-selector",
+      },
+      {
+        type: "mp--quantity-selector",
+      },
+      {
+        type: "mp--atc-buttons",
+        addToCartText: "Add to cart",
+        addBundleToCartText: "Add bundle to cart",
+        soldOutText: "Sold out",
+        showShopPayButton: true,
+        buttonClassName: "w-full uppercase",
+      },
+      {
+        type: "mp--details-accordion",
+        showShippingPolicy: true,
+        showRefundPolicy: true,
+      },
+    ],
   },
 });
