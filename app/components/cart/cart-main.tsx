e33728f93712dbab751e2ce5e537b7c9c1ffce3a
hta218 <hta218.dev@gmail.com>
2025-11-03 16:07:24 +0700

    Restructure main cart component

--- app/components/cart/cart-main.tsx
+++ app/components/cart/cart-main.tsx
@@ -1,16 +1,72 @@
-import { type OptimisticCart, useOptimisticCart } from "@shopify/hydrogen";
+import { useOptimisticCart } from "@shopify/hydrogen";
 import clsx from "clsx";
 import { useRef } from "react";
 import useScroll from "react-use/esm/useScroll";
 import type { CartApiQueryFragment } from "storefront-api.generated";
+import { Link } from "~/components/link";
 import { ScrollArea } from "~/components/scroll-area";
+import { Section } from "~/components/section";
 import type { CartLayoutType } from "~/types/others";
-import { CartDiscounts } from "./cart-discounts";
-import { CartEmpty } from "./cart-empty";
+import { CartBestSellers } from "./cart-best-sellers";
 import { CartLineItem } from "./cart-line-item";
-import { CartCheckoutActions, CartSummary } from "./cart-summary";
+import { CartSummary } from "./cart-summary";
 
-type CartLine = OptimisticCart<CartApiQueryFragment>["lines"]["nodes"][0];
+function CartEmpty({
+  hidden = false,
+  layout = "drawer",
+  onClose,
+}: {
+  hidden: boolean;
+  layout?: CartLayoutType;
+  onClose?: () => void;
+}) {
+  const scrollRef = useRef(null);
+  const { y } = useScroll(scrollRef);
+  return (
+    <div
+      ref={scrollRef}
+      className={clsx(
+        layout === "drawer" && [
+          "h-screen-dynamic w-[400px] flex flex-col justify-center text-center content-start space-y-12 overflow-y-scroll px-5 pb-5 transition",
+          y > 0 && "border-t",
+        ],
+        layout === "page" && [
+          "w-full gap-4 pb-12 md:items-start md:gap-8 lg:gap-12",
+        ],
+      )}
+      hidden={hidden}
+    >
+      <div className={clsx(layout === "page" && "text-center")}>
+        <p className="mb-4">
+          Looks like you haven&rsquo;t added anything yet, let&rsquo;s get you
+          started!
+        </p>
+        <Link
+          variant="outline"
+          to="/products"
+          className={clsx(
+            layout === "drawer" ? "w-full" : "min-w-48",
+            "justify-center",
+          )}
+          onClick={onClose}
+        >
+          Start Shopping
+        </Link>
+      </div>
+      {layout === "page" && (
+        <Section width="fixed" verticalPadding="medium">
+          <div className="grid gap-4">
+            <CartBestSellers
+              count={4}
+              heading="Shop Best Sellers"
+              sortKey="BEST_SELLING"
+            />
+          </div>
+        </Section>
+      )}
+    </div>
+  );
+}
 
 export function CartMain({
   layout,
@@ -21,6 +77,8 @@ export function CartMain({
   onClose?: () => void;
   cart: CartApiQueryFragment;
 }) {
+  const scrollRef = useRef(null);
+  const { y } = useScroll(scrollRef);
   const cart = useOptimisticCart<CartApiQueryFragment>(originalCart);
   const linesCount = Boolean(cart?.lines?.nodes?.length || 0);
   const cartHasItems = Boolean(cart) && cart.totalQuantity > 0;
@@ -28,83 +86,45 @@ export function CartMain({
   return (
     <>
       <CartEmpty hidden={linesCount} onClose={onClose} layout={layout} />
-      <CartDetails cart={cart} layout={layout} cartHasItems={cartHasItems} />
-    </>
-  );
-}
-
-function CartDetails({
-  layout,
-  cart,
-  cartHasItems,
-}: {
-  layout: CartLayoutType;
-  cart: OptimisticCart<CartApiQueryFragment>;
-  cartHasItems: boolean;
-}) {
-  return (
-    <div
-      className={clsx(
-        layout === "drawer" &&
-          "grid grow grid-cols-1 grid-rows-[1fr_auto] px-4",
-        layout === "page" && [
-          "mx-auto w-full max-w-(--page-width) pb-12",
-          "grid md:grid-cols-2 md:items-start lg:grid-cols-3",
-          "gap-8 md:gap-8 lg:gap-12",
-        ],
-      )}
-    >
-      <CartLines lines={cart?.lines?.nodes ?? []} layout={layout} />
-      {cartHasItems && (
-        <CartSummary
-          cost={cart.cost}
-          layout={layout}
-          isOptimistic={cart?.isOptimistic}
-        >
-          <CartDiscounts discountCodes={cart.discountCodes} />
-          <CartCheckoutActions checkoutUrl={cart.checkoutUrl} layout={layout} />
-        </CartSummary>
-      )}
-    </div>
-  );
-}
-
-function CartLines({
-  layout = "drawer",
-  lines,
-}: {
-  layout: CartLayoutType;
-  lines: CartLine[];
-}) {
-  const scrollRef = useRef(null);
-  const { y } = useScroll(scrollRef);
-
-  return (
-    <div
-      ref={scrollRef}
-      className={clsx([
-        "-mx-4 pb-4",
-        y > 0 ? "border-line-subtle border-t" : "",
-        layout === "page" && "grow md:translate-y-4 lg:col-span-2",
-        layout === "drawer" && "transition",
-      ])}
-    >
-      <ScrollArea
-        className={clsx(layout === "drawer" && "max-h-[calc(100vh-312px)]")}
-        size="sm"
+      <div
+        className={clsx(
+          layout === "drawer" &&
+            "grid grow grid-cols-1 grid-rows-[1fr_auto] px-4",
+          layout === "page" && [
+            "mx-auto w-full max-w-(--page-width) pb-12",
+            "grid lg:grid-cols-[1fr_480px] md:items-start",
+            "gap-8 md:gap-8 lg:gap-12",
+          ],
+        )}
       >
-        <ul
-          className={clsx(
-            "grid px-4",
-            layout === "page" && "gap-9",
-            layout === "drawer" && "gap-5",
-          )}
+        <div
+          ref={scrollRef}
+          className={clsx([
+            "-mx-4 pb-4",
+            y > 0 ? "border-line-subtle border-t" : "",
+            layout === "page" && "grow md:translate-y-4",
+            layout === "drawer" && "transition",
+          ])}
         >
-          {lines.map((line) => (
-            <CartLineItem key={line.id} line={line} layout={layout} />
-          ))}
-        </ul>
-      </ScrollArea>
-    </div>
+          <ScrollArea
+            className={clsx(layout === "drawer" && "max-h-[calc(100vh-312px)]")}
+            size="sm"
+          >
+            <ul
+              className={clsx(
+                "grid px-4",
+                layout === "page" && "gap-9",
+                layout === "drawer" && "gap-5",
+              )}
+            >
+              {(cart?.lines?.nodes ?? []).map((line) => (
+                <CartLineItem key={line.id} line={line} layout={layout} />
+              ))}
+            </ul>
+          </ScrollArea>
+        </div>
+        {cartHasItems && <CartSummary cart={cart} layout={layout} />}
+      </div>
+    </>
   );
 }
