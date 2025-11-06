a8436b133631ec4d04cf5aa975a2dd3eec40021d
hta218 <hta218.dev@gmail.com>
2025-11-06 16:55:06 +0700

    Refactor cart summary actions and add gift card redemption functionality

--- app/components/cart/cart-summary-actions.tsx
+++ app/components/cart/cart-summary-actions.tsx
@@ -8,52 +8,13 @@ import { Banner } from "~/components/banner";
 import { Button } from "~/components/button";
 import { cn } from "~/utils/cn";
 
-export function CartSummaryActions({
-  discountCodes,
-  cartNote,
-}: {
-  discountCodes: CartApiQueryFragment["discountCodes"];
-  cartNote: string;
-}) {
-  const [noteDialogOpen, setNoteDialogOpen] = useState(false);
-  const [discountDialogOpen, setDiscountDialogOpen] = useState(false);
-
-  return (
-    <div className="flex items-center justify-end gap-2">
-      <Dialog.Root open={noteDialogOpen} onOpenChange={setNoteDialogOpen}>
-        <Dialog.Trigger asChild>
-          <Button variant="underline">Add a note</Button>
-        </Dialog.Trigger>
-        <NoteDialog cartNote={cartNote} onOpenChange={setNoteDialogOpen} />
-      </Dialog.Root>
-      <span>/</span>
-      <Dialog.Root
-        open={discountDialogOpen}
-        onOpenChange={setDiscountDialogOpen}
-      >
-        <Dialog.Trigger asChild>
-          <Button variant="underline">Apply a discount</Button>
-        </Dialog.Trigger>
-        <DiscountDialog
-          discountCodes={discountCodes}
-          onOpenChange={setDiscountDialogOpen}
-        />
-      </Dialog.Root>
-    </div>
-  );
-}
-
-function NoteDialog({
-  onOpenChange,
-  cartNote: currentNote,
-}: {
-  onOpenChange: (open: boolean) => void;
-  cartNote: string;
-}) {
+export function NoteDialog({ cartNote: currentNote }: { cartNote: string }) {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const fetcher = useFetcher();
   const cartNote = textareaRef.current?.value || "";
-  const submitted = cartNote && fetcher.state === "idle" && fetcher.data;
+  const submitted = Boolean(
+    cartNote && fetcher.state === "idle" && fetcher.data,
+  );
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -84,7 +45,7 @@ function NoteDialog({
         )}
         aria-describedby={undefined}
       >
-        <div className="relative w-full max-w-md overflow-hidden bg-white shadow-xl p-6">
+        <div className="relative w-full max-w-md overflow-hidden bg-white p-6 shadow-xl">
           <Dialog.Close asChild>
             <button
               type="button"
@@ -95,14 +56,14 @@ function NoteDialog({
             </button>
           </Dialog.Close>
 
-          <Dialog.Title className="font-medium text-lg mb-4">
+          <Dialog.Title className="mb-4 font-medium text-lg">
             Add a note
           </Dialog.Title>
 
           <form className="space-y-4" onSubmit={handleSubmit}>
             <textarea
               ref={textareaRef}
-              className="w-full p-3 min-h-20 resize-none"
+              className="min-h-20 w-full resize-none p-3"
               placeholder="Add any special instructions or notes for your order..."
               rows={4}
               name="cartNote"
@@ -112,18 +73,16 @@ function NoteDialog({
               <Banner variant="success">Cart note saved successfully 🎉</Banner>
             )}
             <div className="flex items-center justify-end gap-3">
-              <Button
-                variant="custom"
-                onClick={() => onOpenChange(false)}
-                className="w-24 border-none"
-              >
-                Cancel
-              </Button>
+              <Dialog.Close asChild>
+                <Button variant="custom" className="w-24 border-none">
+                  Cancel
+                </Button>
+              </Dialog.Close>
               <Button
                 type="submit"
                 loading={fetcher.state !== "idle"}
                 disabled={fetcher.state !== "idle"}
-                className="leading-tight! w-24 [--spinner-duration:400ms]"
+                className="w-24 leading-tight! [--spinner-duration:400ms]"
               >
                 Save note
               </Button>
@@ -135,19 +94,17 @@ function NoteDialog({
   );
 }
 
-function DiscountDialog({
+export function DiscountDialog({
   discountCodes = [],
-  onOpenChange,
 }: {
   discountCodes: CartApiQueryFragment["discountCodes"];
-  onOpenChange: (open: boolean) => void;
 }) {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [code, setCode] = useState("");
   const fetcher = useFetcher();
-  const code = inputRef.current?.value || "";
-  const submitted = code && fetcher.state === "idle" && fetcher.data;
-  const success =
-    submitted && discountCodes?.find((d) => d.code === code && d.applicable);
+  const submitted = Boolean(code && fetcher.state === "idle" && fetcher.data);
+  const success = Boolean(
+    submitted && discountCodes?.find((d) => d.code === code && d.applicable),
+  );
   const error = submitted && !success;
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -174,7 +131,11 @@ function DiscountDialog({
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 z-50 bg-gray-900/50 data-[state=open]:animate-fade-in" />
       <Dialog.Content
-        onCloseAutoFocus={(e) => e.preventDefault()}
+        onCloseAutoFocus={(e) => {
+          e.preventDefault();
+          setCode("");
+          fetcher.data = null;
+        }}
         className={cn(
           "fixed inset-0 z-50 flex items-center justify-center p-4 backdrop-blur-xs",
           "[--slide-up-from:20px]",
@@ -182,7 +143,7 @@ function DiscountDialog({
         )}
         aria-describedby={undefined}
       >
-        <div className="relative w-full max-w-md overflow-hidden bg-white shadow-xl p-6">
+        <div className="relative w-full max-w-md overflow-hidden bg-white p-6 shadow-xl">
           <Dialog.Close asChild>
             <button
               type="button"
@@ -193,14 +154,18 @@ function DiscountDialog({
             </button>
           </Dialog.Close>
 
-          <Dialog.Title className="font-medium text-xl mb-4">
+          <Dialog.Title className="mb-4 font-medium text-xl">
             Apply a discount code
           </Dialog.Title>
 
           <form onSubmit={handleSubmit} className="space-y-4">
             <input
-              ref={inputRef}
-              className="p-3 w-full"
+              value={code}
+              onChange={(e) => {
+                setCode(e.target.value);
+                fetcher.data = null;
+              }}
+              className="w-full p-3"
               type="text"
               name="discountCode"
               placeholder="Discount code"
@@ -213,22 +178,135 @@ function DiscountDialog({
             )}
             {error && <Banner variant="error">Invalid discount code.</Banner>}
             <div className="flex items-center justify-end gap-3">
-              <Button
-                variant="custom"
-                onClick={() => {
-                  onOpenChange(false);
-                }}
-                className="w-24 border-none"
-              >
-                Cancel
-              </Button>
+              <Dialog.Close asChild>
+                <Button variant="custom" className="w-24 border-none">
+                  Cancel
+                </Button>
+              </Dialog.Close>
               <Button
                 type="submit"
-                className="leading-tight! w-24 [--spinner-duration:400ms]"
+                className="w-24 leading-tight! [--spinner-duration:400ms]"
                 loading={fetcher.state !== "idle"}
                 disabled={fetcher.state !== "idle"}
               >
                 Apply
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
+}
+
+export function GiftCardDialog({
+  appliedGiftCards = [],
+}: {
+  appliedGiftCards: CartApiQueryFragment["appliedGiftCards"];
+}) {
+  const appliedGiftCardCodes = useRef<string[]>([]);
+  const [code, setCode] = useState("");
+  const fetcher = useFetcher();
+  const submitted = Boolean(code && fetcher.state === "idle" && fetcher.data);
+  const success = Boolean(
+    submitted &&
+      appliedGiftCards?.find((gc) =>
+        code.toLowerCase().endsWith(gc.lastCharacters),
+      ),
+  );
+  const error = submitted && !success;
+
+  function saveAppliedCode(gcCode: string) {
+    const formattedCode = gcCode.replace(/\s/g, ""); // Remove spaces
+    if (!appliedGiftCardCodes.current.includes(formattedCode)) {
+      appliedGiftCardCodes.current.push(formattedCode);
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const giftCardCode = formData.get("giftCardCode") as string;
+    if (giftCardCode) {
+      fetcher.submit(
+        {
+          [CartForm.INPUT_NAME]: JSON.stringify({
+            action: CartForm.ACTIONS.GiftCardCodesUpdate,
+            inputs: {
+              giftCardCode,
+              giftCardCodes: appliedGiftCardCodes.current,
+            },
+          }),
+        },
+        { method: "POST", action: "/cart" },
+      );
+      saveAppliedCode(giftCardCode);
+    }
+  }
+
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 z-50 bg-gray-900/50 data-[state=open]:animate-fade-in" />
+      <Dialog.Content
+        onCloseAutoFocus={(e) => {
+          e.preventDefault();
+          setCode("");
+          fetcher.data = null;
+        }}
+        className={cn(
+          "fixed inset-0 z-50 flex items-center justify-center p-4 backdrop-blur-xs",
+          "[--slide-up-from:20px]",
+          "data-[state=open]:animate-slide-up",
+        )}
+        aria-describedby={undefined}
+      >
+        <div className="relative w-full max-w-md overflow-hidden bg-white p-6 shadow-xl">
+          <Dialog.Close asChild>
+            <button
+              type="button"
+              className="absolute top-2 right-2 z-10 flex h-8 w-8 items-center justify-center rounded-full bg-white/80 backdrop-blur transition-colors hover:bg-gray-100 focus-visible:outline-0"
+              aria-label="Close"
+            >
+              <XIcon size={16} />
+            </button>
+          </Dialog.Close>
+
+          <Dialog.Title className="mb-4 font-medium text-xl">
+            Redeem a gift card
+          </Dialog.Title>
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <input
+              className="w-full p-3"
+              type="text"
+              name="giftCardCode"
+              placeholder="Gift card code"
+              value={code}
+              onChange={(e) => {
+                setCode(e.target.value);
+                fetcher.data = null;
+              }}
+              required
+            />
+            {success && (
+              <Banner variant="success">
+                Gift card applied successfully 🎉
+              </Banner>
+            )}
+            {error && <Banner variant="error">Invalid gift card code.</Banner>}
+            <div className="flex items-center justify-end gap-3">
+              <Dialog.Close asChild>
+                <Button variant="custom" className="w-24 border-none">
+                  Cancel
+                </Button>
+              </Dialog.Close>
+              <Button
+                type="submit"
+                className="w-24 leading-tight! [--spinner-duration:400ms]"
+                loading={fetcher.state !== "idle"}
+                disabled={fetcher.state !== "idle"}
+              >
+                Redeem
               </Button>
             </div>
           </form>
