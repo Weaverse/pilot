71a7371cd75a7da7d6b050fe7631c4f3339b2d81
Paul <phanminh65@gmail.com>
2026-03-18 17:46:45 +0700

    perf: add default CacheShort fallback to routeHeaders

--- app/routes/catch-all.tsx
+++ app/routes/catch-all.tsx
@@ -1,5 +1,8 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { validateWeaverseData, WeaverseContent } from "~/weaverse";
+import { routeHeaders } from "~/utils/cache";
+
+export let headers = routeHeaders;
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const weaverseData = await context.weaverse.loadPage({
