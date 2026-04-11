f91ae43065f228a52f3fbeac9bd4bdf0bd3eea09
hta218 <hta218.dev@gmail.com>
2026-04-11 21:27:52 +0700

    Update heading tag class in ProductTitle component for improved styling

--- app/sections/main-product/product-title.tsx
+++ app/sections/main-product/product-title.tsx
@@ -17,7 +17,7 @@ export default function ProductTitle(props: ProductTitleProps) {
 
   return (
     <div {...rest}>
-      <Tag className="h3 tracking-tight!">{product.title}</Tag>
+      <Tag className="h2 tracking-tight!">{product.title}</Tag>
     </div>
   );
 }
